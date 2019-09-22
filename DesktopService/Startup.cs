b5cb140454d0ff6029d4d6ddcc5f1ef608745b3f
Christoph Taucher <taucher.ch@gmail.com>
2019-09-22 12:51:59 +0200

    configure identity & authentication

--- DesktopService/Startup.cs
+++ DesktopService/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using DesktopService.Features.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -26,6 +28,32 @@ namespace DesktopService
         // Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var identitySettingsSection = Configuration.GetSection("IdentitySettings");
+            services.Configure<Features.Identity.Models.IdentitySettings>(identitySettingsSection);
+
+            var appSettings = identitySettingsSection.Get<Features.Identity.Models.IdentitySettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            // configure DI for application services
+            services.AddScoped<Features.Identity.IUserService, Features.Identity.UserService>();
+
             services.AddHostedService<Worker>();
             services.AddSignalR();
             services.AddLogging();
@@ -38,6 +66,8 @@ namespace DesktopService
         // Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseAuthentication();
+
             app.UseSignalR(route =>
             {
                 route.MapHub<ConnectHub>("/connect");
