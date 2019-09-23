adc379d4be9ec75ac0363043dc12bc54e3392ccf
Christoph Taucher <taucher.ch@gmail.com>
2019-09-23 19:45:21 +0200

    add library for ipc named pipes

---
+++ IPCPipe/IPipeServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCPipe
+{
+    public interface IPipeServer
+    {
+        event EventHandler<object> DataReceived;
+        void Listen(string hub, Action<object> callback = null);
+        void Send(string hub, object data);
+        void Disconnect(string hub = null);
+    }
+}
---
+++ IPCPipe/PipeServer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPCPipe
+{
+    public class PipeServer : IPipeServer
+    {
+        public event EventHandler<object> DataReceived;
+
+        private Dictionary<string, NamedPipeServerStream> hubs = new Dictionary<string, NamedPipeServerStream>();
+
+        public void Listen(string hub, Action<object> callback = null)
+        {
+            if (!hubs.ContainsKey(hub))
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    try
+                    {
+                        var server = OnCreateHub(hub);
+                        using (StreamReader reader = new StreamReader(server))
+                        {
+                            while (true)
+                            {
+                                var result = reader.ReadToEnd();
+                                if (!string.IsNullOrWhiteSpace(result))
+                                {
+                                    var obj = Newtonsoft.Json.JsonConvert.DeserializeObject(result);
+                                    DataReceived?.Invoke(this, obj);
+                                    callback?.Invoke(obj);
+                                }
+                            }
+                        }
+                    } finally
+                    {
+                        if (hubs.ContainsKey(hub))
+                            hubs.Remove(hub);
+                    }
+                });
+            }
+        }
+
+        public void Send(string hub, object data)
+        {
+            if (data != null)
+            {
+                if (hubs.ContainsKey(hub))
+                {
+                    using (StreamWriter writer = new StreamWriter(hubs[hub]))
+                    {
+                        writer.Write(Newtonsoft.Json.JsonConvert.SerializeObject(data));
+                        writer.Flush();
+                    }
+                } else
+                {
+                    using (StreamWriter writer = new StreamWriter(OnCreateHub(hub)))
+                    {
+                        writer.Write(Newtonsoft.Json.JsonConvert.SerializeObject(data));
+                        writer.Flush();
+                    }
+                }
+            }
+        }
+
+        public void Disconnect(string hub = null)
+        {
+            if (string.IsNullOrWhiteSpace(hub))
+            {
+                foreach (var item in hubs)
+                {
+                    item.Value.Disconnect();
+                    item.Value.Dispose();
+                }
+                hubs.Clear();
+            } else
+            {
+                if (hubs.ContainsKey(hub))
+                {
+                    hubs[hub].Disconnect();
+                    hubs[hub].Dispose();
+                    hubs.Remove(hub);
+                }
+            }
+        }
+
+        private NamedPipeServerStream OnCreateHub(string hub)
+        {
+            var server = new NamedPipeServerStream(hub);
+            server.WaitForConnection();
+            hubs.Add(hub, server);
+            return server;
+        }
+    }
+}
