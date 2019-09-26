379da353ed945a3400051222b92d210e92ba2bf3
Christoph Taucher <taucher.ch@gmail.com>
2019-09-26 09:50:31 +0200

    add user control for devices and features

---
+++ DesktopHub/Devices/DevicesControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DesktopHub.Devices
+{
+    public class DevicesControl : UserControl
+    {
+        public DevicesControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ DesktopHub/Features/FeaturesControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DesktopHub.Features
+{
+    public class FeaturesControl : UserControl
+    {
+        public FeaturesControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
