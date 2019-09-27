207fc375b164e3196a870ce427bfe74492fcc008
Christoph Taucher <taucher.ch@gmail.com>
2019-09-27 11:46:57 +0200

    dev. device feature

---
+++ SharedCoreModels/DeviceFeature/DeviceFeature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedCoreModels.DeviceFeature
+{
+    public class DeviceFeature
+    {
+        public enum IntegrationPoint
+        {
+            Desktop = 0,
+            Mobile = 1
+        }
+
+        public IntegrationPoint FeatureIntegrationPoint { get; set; } = IntegrationPoint.Desktop;
+        public IntegrationPoint ControlIntegrationPoint { get; set; } = IntegrationPoint.Mobile;
+        public string DisplayName { get; set; }
+        public Guid Id { get; set; }
+        public Version Version { get; set; }
+        public Version MinFeatureIntegrationPoint { get; set; }
+        public Version MinControlIntegrationPoint { get; set; }
+        public IFeatureIntegrationContent FeatureIntegrationContent { get; set; }
+        public IControlIntegrationContent ControlIntegrationContent { get; set; }
+    }
+}
