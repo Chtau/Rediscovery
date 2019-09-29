00407849bc9b7951d3eab5a69af14632e5cd8ad2
Christoph Taucher <taucher.ch@gmail.com>
2019-09-29 11:17:44 +0200

    change tabbar to scrollable

--- Rediscovery/Rediscovery/Views/MainPage.xaml.cs
+++ Rediscovery/Rediscovery/Views/MainPage.xaml.cs
@@ -33,11 +33,13 @@ namespace Rediscovery.Views
                 foreach (var item in e.NewItems)
                 {
                     var model = (Features.Authentication.Models.Connection)item;
-                    var navigationPage = new NavigationPage(new Desktops.DesktopPage());
-                    navigationPage.IconImageSource = "tab_feed.png";
-                    navigationPage.Title = model.DisplayName;
-
-                    Children.Add(navigationPage);
+                    var navigationPage = new NavigationPage(new Desktops.DesktopPage())
+                    {
+                        IconImageSource = "tab_feed.png",
+                        Title = model.DisplayName
+                    };
+                    Children.Insert(0, navigationPage);
+                    //Children.Add(navigationPage);
                 }
             }
         }
