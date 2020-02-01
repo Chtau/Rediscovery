9c49e311eb814b7d21cc2bd8c1d2e910dc87f8ab
Christoph Taucher <taucher.ch@gmail.com>
2020-02-01 16:10:50 +0100

    add base feature view model

---
+++ Rediscovery/Rediscovery/Features/DesktopFeatures/FeaturePage/BaseFeatureViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Rediscovery.Services;
+using Rediscovery.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Rediscovery.Features.DesktopFeatures.FeaturePage
+{
+    public class BaseFeatureViewModel : BaseViewModel
+    {
+        private IFeatureExchange featureExchange => DependencyService.Get<IFeatureExchange>() ?? new FeatureExchange();
+
+        internal readonly Authentication.Models.ConnectionManifestFeature _connectionManifestFeature;
+        internal ILogger logger => DependencyService.Get<ILogger>() ?? new Logger();
+
+        public BaseFeatureViewModel(Authentication.Models.ConnectionManifestFeature connectionManifestFeature)
+        {
+            _connectionManifestFeature = connectionManifestFeature;
+            featureExchange.DesktopResponseReceived += FeatureExchange_DesktopResponseReceived;
+        }
+
+        private void FeatureExchange_DesktopResponseReceived(object sender, (Guid connectionId, Guid featureId, object data) e)
+        {
+            if (_connectionManifestFeature.ConnectionId == e.connectionId && _connectionManifestFeature.FeatureId == e.featureId)
+            {
+                Receive(e.data);
+            }
+        }
+
+        internal virtual void Send(object data)
+        {
+            featureExchange.Send(_connectionManifestFeature, data);
+        }
+
+        internal virtual void Receive(object data)
+        {
+
+        }
+    }
+}
