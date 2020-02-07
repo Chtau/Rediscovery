75994b4d906983d7b152627a1b13495ceaeceaa5
Christoph Taucher <taucher.ch@gmail.com>
2020-02-07 20:24:20 +0100

    add register for registered devices

--- SharedCoreModels/DeviceFeature/BaseDeviceFeature.cs
+++ SharedCoreModels/DeviceFeature/BaseDeviceFeature.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SharedCoreModels.DeviceFeature
 {
     public abstract class BaseDeviceFeature : IDeviceFeatureImplementation
     {
+        internal class RegisteredDevice
+        {
+            internal string DeviceId { get; set; }
+
+            internal DateTime RegisterDate { get; set; }
+
+            public RegisteredDevice(string deviceId)
+            {
+                DeviceId = deviceId;
+                RegisterDate = DateTime.UtcNow;
+            }
+        }
+
+        internal List<RegisteredDevice> registeredDevices = new List<RegisteredDevice>();
+
+        internal bool IsRegister(string deviceId)
+        {
+            return registeredDevices.Any(x => string.Equals(x.DeviceId, deviceId, StringComparison.OrdinalIgnoreCase));
+        }
+
         public event EventHandler<DeviceFeatureData> SendData;
 
         public void OnSendData(object obj, DeviceFeatureData args)
@@ -35,12 +56,17 @@ namespace SharedCoreModels.DeviceFeature
 
         public virtual void Register(string deviceId)
         {
-            
+            if (!IsRegister(deviceId))
+                registeredDevices.Add(new RegisteredDevice(deviceId));
         }
 
         public virtual void Unregister(string deviceId)
         {
-            
+            if (IsRegister(deviceId))
+            {
+                var item = registeredDevices.First(x => string.Equals(x.DeviceId, deviceId, StringComparison.OrdinalIgnoreCase));
+                registeredDevices.Remove(item);
+            }
         }
     }
 }
