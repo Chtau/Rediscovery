717053a2753a6ab2974e0014ac73f4ac88e85f8e
Christoph Taucher <taucher.ch@gmail.com>
2020-02-09 13:37:06 +0100

    add media player profile for plex

--- DesktopFeatureMediaPlayer/MediaPlayerDefaultProfiles.cs
+++ DesktopFeatureMediaPlayer/MediaPlayerDefaultProfiles.cs
@@ -12,6 +12,7 @@ namespace DesktopFeatureMediaPlayer
         {
             var retVal = new List<ProfileConfiguration>();
             retVal.Add(VLC());
+            retVal.Add(Plex());
             return retVal;
         }
 
@@ -43,6 +44,40 @@ namespace DesktopFeatureMediaPlayer
                 DisplayName = "VLC",
                 Id = vlcId,
                 ProcessName = "vlc",
+                CommandConfiguration = vlcCmdConfig
+            };
+            profile.CommandAvailable = vlcCmdConfig.GetConfiguratedCommands();
+            return profile;
+        }
+
+        private static ProfileConfiguration Plex()
+        {
+            var vlcId = new Guid("CC9033CA-02CE-48A6-8393-1C4838D9C1E8");
+            var vlcCmdConfig = new CommandConfiguration
+            {
+                ProfileId = vlcId,
+                CommandKeys = new Dictionary<CommandConfiguration.CommandTypes, SharedCoreModels.FeatureModels.KeyCodes.KeyCode[]>()
+            };
+            vlcCmdConfig.CommandKeys.Add(CommandConfiguration.CommandTypes.None, null);
+            vlcCmdConfig.CommandKeys.Add(CommandConfiguration.CommandTypes.Fullscreen, new KeyCode[] { KeyCode.SHIFT, KeyCode.F11 });
+            vlcCmdConfig.CommandKeys.Add(CommandConfiguration.CommandTypes.PlayPause, new KeyCode[] { KeyCode.SPACE_BAR });
+            vlcCmdConfig.CommandKeys.Add(CommandConfiguration.CommandTypes.FullscreenExit, new KeyCode[] { KeyCode.SHIFT, KeyCode.F11 });
+            vlcCmdConfig.CommandKeys.Add(CommandConfiguration.CommandTypes.Next, new KeyCode[] { KeyCode.RIGHT, KeyCode.DECIMAL });
+            vlcCmdConfig.CommandKeys.Add(CommandConfiguration.CommandTypes.Previous, new KeyCode[] { KeyCode.LEFT, KeyCode.OEM_COMMA });
+            //vlcCmdConfig.CommandKeys.Add(CommandConfiguration.CommandTypes.Stop, new KeyCode[] { KeyCode.KEY_S });
+            //vlcCmdConfig.CommandKeys.Add(CommandConfiguration.CommandTypes.Mute, new KeyCode[] { KeyCode.KEY_M });
+            vlcCmdConfig.CommandKeys.Add(CommandConfiguration.CommandTypes.VolumneUp, new KeyCode[] { KeyCode.UP });
+            vlcCmdConfig.CommandKeys.Add(CommandConfiguration.CommandTypes.VolumneDown, new KeyCode[] { KeyCode.DOWN });
+            //vlcCmdConfig.CommandKeys.Add(CommandConfiguration.CommandTypes.SpeedSlower, new KeyCode[] { KeyCode.OEM_MINUS });
+            //vlcCmdConfig.CommandKeys.Add(CommandConfiguration.CommandTypes.SpeedFaster, new KeyCode[] { KeyCode.OEM_PLUS });
+            vlcCmdConfig.CommandKeys.Add(CommandConfiguration.CommandTypes.JumpForward, new KeyCode[] { KeyCode.RIGHT });
+            vlcCmdConfig.CommandKeys.Add(CommandConfiguration.CommandTypes.JumpBackward, new KeyCode[] { KeyCode.LEFT });
+            var profile = new ProfileConfiguration
+            {
+                ApplicationPath = null,
+                DisplayName = "Plex",
+                Id = vlcId,
+                ProcessName = "PlexMediaPlayer",
                 CommandConfiguration = vlcCmdConfig
             };
             profile.CommandAvailable = vlcCmdConfig.GetConfiguratedCommands();
