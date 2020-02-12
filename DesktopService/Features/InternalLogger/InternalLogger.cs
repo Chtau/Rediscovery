6a31855aaa11810d96671c478876a20f6b44e86e
Christoph Taucher <taucher.ch@gmail.com>
2020-02-12 20:36:03 +0100

    change logger

--- DesktopService/Features/InternalLogger/InternalLogger.cs
+++ DesktopService/Features/InternalLogger/InternalLogger.cs
@@ -37,7 +37,7 @@ namespace DesktopService.Features.InternalLogger
             {
                 var color = Console.ForegroundColor;
                 Console.ForegroundColor = _config.Color;
-                Console.WriteLine($"{logLevel.ToString()} - {eventId.Id} - {_name} - {formatter(state, exception)}");
+                Console.WriteLine($"{logLevel.ToString()} - {eventId.Id} - {_name}{Environment.NewLine}{formatter(state, exception)}");
                 Console.ForegroundColor = color;
             }
         }
