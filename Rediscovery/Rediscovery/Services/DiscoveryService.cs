6c97c5a6e1fac54d945fcfcfbbbaeede280cdcdc
Christoph Taucher <taucher.ch@gmail.com>
2020-02-18 19:49:54 +0100

    add discovery service

---
+++ Rediscovery/Rediscovery/Services/DiscoveryService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: Xamarin.Forms.Dependency(typeof(Rediscovery.Services.DiscoveryService))]
+namespace Rediscovery.Services
+{
+    public class DiscoveryService : IDiscoveryService
+    {
+        public void Boardcast(Action<string> callbackAnswer)
+        {
+            var ServerEp = new IPEndPoint(IPAddress.Any, 0);
+            Task.Run(() =>
+            {
+                do
+                {
+                    var Client = new UdpClient();
+                    var ServerResponseData = Client.Receive(ref ServerEp);
+                    var ServerResponse = Encoding.ASCII.GetString(ServerResponseData);
+                    callbackAnswer?.Invoke(ServerEp.Address?.ToString() + " Response:" + ServerResponse);
+
+                    Client.Close();
+                } while (true);
+            });
+            Task.Run(async () =>
+            {
+                do
+                {
+                    await Task.Delay(1000);
+                    var Client = new UdpClient();
+                    var RequestData = Encoding.ASCII.GetBytes("RediscoveryClient");
+
+                    Client.EnableBroadcast = true;
+                    Client.Send(RequestData, RequestData.Length, new IPEndPoint(IPAddress.Broadcast, 8888));
+
+                    Client.Close();
+                } while (true);
+            });
+        }
+    }
+}
---
+++ Rediscovery/Rediscovery/Services/IDiscoveryService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rediscovery.Services
+{
+    public interface IDiscoveryService
+    {
+        void Boardcast(Action<string> callbackAnswer);
+    }
+}
