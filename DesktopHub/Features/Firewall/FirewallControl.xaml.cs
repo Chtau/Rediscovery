6d1d041589930470e3064d33fdfa58fd3c9389bf
Christoph Taucher <taucher.ch@gmail.com>
2020-03-05 13:36:26 +0100

    add firewall control to hub

---
+++ DesktopHub/Features/Firewall/FirewallControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DesktopHub.Features.Firewall
+{
+    public class FirewallControl : UserControl
+    {
+        private readonly FirewallControlViewModel _viewModel;
+
+        public FirewallControl()
+        {
+            this.InitializeComponent();
+            DataContext = _viewModel = new FirewallControlViewModel();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ DesktopHub/Features/Firewall/FirewallControlViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Avalonia;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace DesktopHub.Features.Firewall
+{
+    public class FirewallControlViewModel : BaseViewModel
+    {
+        bool serviceFWExists = false;
+        public bool ServiceFWExists
+        {
+            get { return serviceFWExists; }
+            set { SetProperty(ref serviceFWExists, value); }
+        }
+
+        bool discoveryFWExists = false;
+        public bool DiscoveryFWExists
+        {
+            get { return discoveryFWExists; }
+            set { SetProperty(ref discoveryFWExists, value); }
+        }
+
+        public FirewallControlViewModel()
+        {
+
+        }
+
+        public void CreateServiceFW()
+        {
+
+        }
+
+        public void CreateDiscoveryFW()
+        {
+
+        }
+
+        private static void Elevate(string filePath, string parameters)
+        {
+            var SelfProc = new ProcessStartInfo
+            {
+                UseShellExecute = true,
+                //WorkingDirectory = Environment.CurrentDirectory,
+                FileName = filePath,
+                Arguments = parameters,
+                Verb = "runas"
+            };
+            try
+            {
+                Process.Start(SelfProc);
+            }
+            catch
+            {
+                System.Diagnostics.Debug.Print("Unable to elevate!" + Environment.NewLine);
+            }
+        }
+    }
+}
