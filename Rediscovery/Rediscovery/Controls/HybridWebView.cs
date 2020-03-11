63324fc6c6824ead0190b6cd89d80ac376c2825a
Christoph Taucher <taucher.ch@gmail.com>
2020-03-11 11:38:24 +0100

    dev hybrid web view

--- Rediscovery/Rediscovery/Controls/HybridWebView.cs
+++ Rediscovery/Rediscovery/Controls/HybridWebView.cs
@@ -5,9 +5,6 @@ using Xamarin.Forms;
 
 namespace Rediscovery.Controls
 {
-    // TODO: add baseUrl for multi file handling on websites
-    // https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/webview?tabs=windows
-
     public class HybridWebView : WebView
     {
         Action<string> action;
@@ -27,19 +24,23 @@ namespace Rediscovery.Controls
         public void SetFolderSource(string directory)
         {
             var source = new HtmlWebViewSource();
-            source.BaseUrl = "file://" + directory + (!directory.EndsWith("/") ? "/" : "");
-            // find start file
-            string startFile = "";
-            if (System.IO.File.Exists(System.IO.Path.Combine(directory, "Index.html")))
-                startFile = System.IO.Path.Combine(directory, "Index.html");
-            else if (System.IO.File.Exists(System.IO.Path.Combine(directory, "index.html")))
-                startFile = System.IO.Path.Combine(directory, "index.html");
-            else if (System.IO.File.Exists(System.IO.Path.Combine(directory, "default.html")))
-                startFile = System.IO.Path.Combine(directory, "default.html");
-            else if (System.IO.File.Exists(System.IO.Path.Combine(directory, "Default.html")))
-                startFile = System.IO.Path.Combine(directory, "Default.html");
-            if (!string.IsNullOrWhiteSpace(startFile))
-                source.Html = System.IO.File.ReadAllText(startFile);
+            if (!string.IsNullOrWhiteSpace(directory) && System.IO.Directory.Exists(directory))
+            {
+                source.BaseUrl = "file://" + directory + (!directory.EndsWith("/") ? "/" : "");
+                // find start file
+                string startFile = "";
+                if (System.IO.File.Exists(System.IO.Path.Combine(directory, "Index.html")))
+                    startFile = System.IO.Path.Combine(directory, "Index.html");
+                else if (System.IO.File.Exists(System.IO.Path.Combine(directory, "index.html")))
+                    startFile = System.IO.Path.Combine(directory, "index.html");
+                else if (System.IO.File.Exists(System.IO.Path.Combine(directory, "default.html")))
+                    startFile = System.IO.Path.Combine(directory, "default.html");
+                else if (System.IO.File.Exists(System.IO.Path.Combine(directory, "Default.html")))
+                    startFile = System.IO.Path.Combine(directory, "Default.html");
+                if (!string.IsNullOrWhiteSpace(startFile))
+                    source.Html = System.IO.File.ReadAllText(startFile);
+            }
+
             Source = source;
         }
 
