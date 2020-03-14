37f4caa82cbb18a4f0041472befd631c34487e1a
Christoph Taucher <taucher.ch@gmail.com>
2020-03-14 20:48:34 +0100

    dev. feature view

--- Rediscovery/Rediscovery/Features/DesktopFeatures/FeaturePage/FeatureView/FeatureViewViewModel.cs
+++ Rediscovery/Rediscovery/Features/DesktopFeatures/FeaturePage/FeatureView/FeatureViewViewModel.cs
@@ -1,20 +1,47 @@
-﻿using Rediscovery.ViewModels;
+﻿using PluginFeature.Models;
+using Rediscovery.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using Xamarin.Forms;
 
 namespace Rediscovery.Features.DesktopFeatures.FeaturePage.FeatureView
 {
-    public class FeatureViewViewModel : BaseViewModel
+    public class FeatureViewViewModel : BaseFeatureViewModel
     {
+        private bool processIsRunning = false;
+
         private DesktopFeatures.IFeatureUIService featureUIService => DependencyService.Get<DesktopFeatures.IFeatureUIService>() ?? new DesktopFeatures.FeatureUIService();
 
         public event EventHandler<Tuple<Guid, string>> UIDataReady;
 
         public readonly Features.Connection.Models.ConnectionManifestFeature ConnectionManifestFeature;
 
-        public FeatureViewViewModel(Features.Connection.Models.ConnectionManifestFeature connectionManifestFeature)
+        public ObservableCollection<DeviceFeatureProfil> Profiles { get; set; } = new ObservableCollection<DeviceFeatureProfil>();
+
+        private DeviceFeatureProfil selectedProfile;
+
+        public DeviceFeatureProfil SelectedProfile
+        {
+            get { return selectedProfile; }
+            set
+            {
+                SetProperty(ref selectedProfile, value);
+                OnProfileChanged();
+            }
+        }
+
+        private string currentTitle;
+
+        public string CurrentTitle
+        {
+            get { return currentTitle; }
+            set { SetProperty(ref currentTitle, value); }
+        }
+
+        public FeatureViewViewModel(Features.Connection.Models.ConnectionManifestFeature connectionManifestFeature): base(connectionManifestFeature)
         {
             ConnectionManifestFeature = connectionManifestFeature;
             featureUIService.SaveUI(ConnectionManifestFeature.FeatureId, (state, directory) =>
@@ -24,6 +51,57 @@ namespace Rediscovery.Features.DesktopFeatures.FeaturePage.FeatureView
                     UIDataReady?.Invoke(this, new Tuple<Guid, string>(ConnectionManifestFeature.FeatureId, directory));
                 }
             });
+            base.ReceivedData += FeatureViewViewModel_ReceivedData;
+            if (connectionManifestFeature.Profiles?.Count > 0)
+            {
+                foreach (var item in connectionManifestFeature.Profiles)
+                {
+                    Profiles.Add(item);
+                }
+                SelectedProfile = connectionManifestFeature.Profiles.First();
+                OnProfileChanged();
+            }
+            else
+            {
+                Title = connectionManifestFeature.FeatureDisplayName;
+            }
+        }
+
+        /*public void Send(CommandTypes cmd)
+        {
+            base.Send(SelectedProfile?.Id, new ClientCommandSendModel(_connectionManifestFeature.FeatureId, SelectedProfile.Id, cmd));
+        }*/
+
+        private void FeatureViewViewModel_ReceivedData(object sender, object e)
+        {
+            /*MediaPlayerStateData stateData = Newtonsoft.Json.JsonConvert.DeserializeObject<MediaPlayerStateData>(e?.ToString());
+            if (stateData != null && SelectedProfile != null && string.Equals(SelectedProfile.Id, stateData.ProfileId, StringComparison.OrdinalIgnoreCase))
+            {
+                if (processIsRunning != stateData.ProcessRunning)
+                {
+                    processIsRunning = stateData.ProcessRunning;
+                    OnChangeCanExecute();
+                }
+                CurrentTitle = stateData.CurrentTitle;
+            }*/
+        }
+
+        private void OnProfileChanged()
+        {
+            processIsRunning = false;
+            CurrentTitle = "";
+            Title = SelectedProfile.DisplayName;
+            if (!string.IsNullOrWhiteSpace(SelectedProfile.ProfileData?.ToString()))
+            {
+                /*try
+                {
+                    Commands = System.Text.Json.JsonSerializer.Deserialize<List<CommandTypes>>(SelectedProfile.ProfileData?.ToString());
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex);
+                }*/
+            }
         }
     }
 }
