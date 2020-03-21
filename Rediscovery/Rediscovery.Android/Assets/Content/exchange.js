c4939395c252e0e2008c5e97502c13566544fa41
Christoph Taucher <taucher.ch@gmail.com>
2020-03-21 18:27:42 +0100

    add functions for exchange js

--- Rediscovery/Rediscovery.Android/Assets/Content/exchange.js
+++ Rediscovery/Rediscovery.Android/Assets/Content/exchange.js
@@ -1,3 +1,16 @@
-﻿function featureSend(data) {
+﻿var modelData = {};
+var callbackFunction = null;
+function internalChangeModel(model) {
+    modelData = model;
+    if (callbackFunction) {
+        callbackFunction(modelData);
+    }
+}
+
+function changeModelListener(callback) {
+    callbackFunction = callback;
+}
+
+function featureSend(data) {
     jsBridge.invokeAction(data);
-};
+}
