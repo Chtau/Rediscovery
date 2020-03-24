e6be1af732d9ec158b9192b2ca6a90dc16903636
Christoph Taucher <taucher.ch@gmail.com>
2020-03-24 11:51:40 +0100

    add exchange to embetted resource

---
+++ Rediscovery/Rediscovery/Assets/exchange.js
@@ -0,0 +1,20 @@
+﻿var modelData = undefined;
+var callbackFunction = null;
+function internalChangeModel(model) {
+    modelData = model;
+    if (callbackFunction) {
+        callbackFunction(modelData);
+    }
+}
+
+function changeModelListener(callback) {
+    callbackFunction = callback;
+}
+
+function currentModel() {
+    return modelData;
+}
+
+function featureSend(data) {
+    jsBridge.invokeAction(data);
+}
