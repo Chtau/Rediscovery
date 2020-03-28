1e7ac842b06bcba5d5c37c6a1e0e599a7669010a
Christoph Taucher <taucher.ch@gmail.com>
2020-03-28 11:18:25 +0100

    add logger component

--- Rediscovery.Desktop.Hub/ClientApp/src/app/app.module.ts
+++ Rediscovery.Desktop.Hub/ClientApp/src/app/app.module.ts
@@ -15,10 +15,12 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { DeviceListComponent } from './device/device-list.component';
 import { FeatureListComponent } from './feature/feature-list.component';
 import { FeatureListOverviewComponent } from './feature/list/feature-list-overview.component';
+import { LoggerComponent } from './logger/logger.component';
 
 const routes: Route[] = [
   { path: 'counter', component: CounterComponent },
   { path: 'fetch-data', component: FetchDataComponent },
+  { path: 'logger', component: LoggerComponent },
   { path: 'devices', component: DeviceListComponent },
   { 
     path: 'features',
@@ -42,7 +44,8 @@ const routes: Route[] = [
     FetchDataComponent,
     DeviceListComponent,
     FeatureListComponent,
-    FeatureListOverviewComponent
+    FeatureListOverviewComponent,
+    LoggerComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
