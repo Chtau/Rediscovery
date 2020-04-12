4120f293e3e845d44935cab3acf9934dfc8e17cf
Christoph Taucher <taucher.ch@gmail.com>
2020-04-12 14:55:08 +0200

    add new Project for App start

---
+++ AppControlPanel/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using AppControlPanel.ViewModels;
+using AppControlPanel.Views;
+
+namespace AppControlPanel
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
