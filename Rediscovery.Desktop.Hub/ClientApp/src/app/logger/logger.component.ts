b17495678f78fdf7c1e3d85cb9f78435531ebad8
Christoph Taucher <taucher.ch@gmail.com>
2020-04-19 15:37:36 +0200

    add filter for logger console

--- Rediscovery.Desktop.Hub/ClientApp/src/app/logger/logger.component.ts
+++ Rediscovery.Desktop.Hub/ClientApp/src/app/logger/logger.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, ElementRef, OnInit, AfterViewChecked, AfterViewInit } from '@angular/core';
 import { LoggerService } from './logger.service';
-import { ILoggerEntry } from './logger.interface';
+import { ILoggerEntry, LoggerType } from './logger.interface';
 
 @Component({
   selector: 'app-logger',
@@ -12,6 +12,7 @@ export class LoggerComponent implements OnInit, AfterViewChecked, AfterViewInit 
 
   autoscroll: boolean = true;
   entries: ILoggerEntry[] = [];
+  sourceEntries: ILoggerEntry[] = [];
 
   showTrace: boolean = true;
   showDebug: boolean = true;
@@ -21,10 +22,12 @@ export class LoggerComponent implements OnInit, AfterViewChecked, AfterViewInit 
   showCritical: boolean = true;
 
   constructor(private loggerService: LoggerService) {
-    this.entries = loggerService.getEntries();
+    this.sourceEntries = loggerService.getEntries();
+    this.setEntires();
     this.scrollToBottom();
     this.loggerService.entryAddedChanged.subscribe(result => {
-      this.entries.push(result);
+      this.sourceEntries.push(result);
+      this.setEntires();
       this.scrollToBottom();
     });
   }
@@ -54,10 +57,63 @@ export class LoggerComponent implements OnInit, AfterViewChecked, AfterViewInit 
 
   clearConsole(): void {
     this.entries = [];
+    this.sourceEntries = [];
   }
 
   changeAutoscroll(): void {
     this.autoscroll = !this.autoscroll;
   }
 
+  filterTrace():void {
+    this.showTrace = !this.showTrace;
+    this.setEntires();
+  }
+
+  filterDebug():void {
+    this.showDebug = !this.showDebug;
+    this.setEntires();
+  }
+  
+  filterInformation():void {
+    this.showInformation = !this.showInformation;
+    this.setEntires();
+  }
+    
+  filterWarning():void {
+    this.showWarning = !this.showWarning;
+    this.setEntires();
+  }
+      
+  filterError():void {
+    this.showError = !this.showError;
+    this.setEntires();
+  }
+        
+  filterCritical():void {
+    this.showCritical = !this.showCritical;
+    this.setEntires();
+  }
+
+  private setEntires(): void {
+    this.entries = this.sourceEntries.filter(item => {
+      if (this.showTrace == true && item.type == LoggerType.Trace) {
+        return true;
+      }
+      if (this.showDebug == true && item.type == LoggerType.Debug) {
+        return true;
+      }
+      if (this.showInformation == true && item.type == LoggerType.Information) {
+        return true;
+      }
+      if (this.showWarning == true && item.type == LoggerType.Warning) {
+        return true;
+      }
+      if (this.showError == true && item.type == LoggerType.Error) {
+        return true;
+      }
+      if (this.showCritical == true && item.type == LoggerType.Critical) {
+        return true;
+      }
+    });
+  }
 }
