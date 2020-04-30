dee9189869403f5c7a9941c55f5c286f94ea266d
Christoph Taucher <taucher.ch@gmail.com>
2020-04-30 21:22:26 +0200

    hub integration for Info Window

--- DesktopService/Features/RemoteResources/RemoteResourcesServiceInfo.cs
+++ DesktopService/Features/RemoteResources/RemoteResourcesServiceInfo.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
@@ -11,21 +12,26 @@ namespace DesktopService.Features.RemoteResources
         private readonly SharedConfigurations.DesktopService.Models.RemoteResourceConfiguration _remoteResourceSettings;
         private readonly ILogger<RemoteResourcesServiceInfo> _logger;
         private readonly SharedConfigurations.DesktopService.Models.AppConfiguration _appSettings;
+        private readonly IHubContext<DesktopInfoHubRemoteResourceHub> _hubContext;
 
         public RemoteResourcesServiceInfo(ILoggerFactory loggerFactory, IOptions<SharedConfigurations.DesktopService.Models.RemoteResourceConfiguration> remoteResourceSettings,
-            IOptions<SharedConfigurations.DesktopService.Models.AppConfiguration> appOptions)
+            IOptions<SharedConfigurations.DesktopService.Models.AppConfiguration> appOptions,
+            IHubContext<DesktopInfoHubRemoteResourceHub> hubContext)
         {
             _logger = loggerFactory.CreateLogger<RemoteResourcesServiceInfo>();
             _remoteResourceSettings = remoteResourceSettings.Value;
             _appSettings = appOptions.Value;
+            _hubContext = hubContext;
         }
 
         public void ShowInfoWindow(bool forceStart = false)
         {
-            // TODO: add pipe or refactor for service startup info
             if (_remoteResourceSettings.ShowServiceInfoOnStart || forceStart)
             {
-                if (!string.IsNullOrWhiteSpace(_remoteResourceSettings.RediscoveryDesktopHubPath))
+                if (ActiveDesktopInfoHandler.ConnectionIds.Count > 0)
+                {
+                    _hubContext.Clients.Group(DesktopInfoHubRemoteResourceHub.GroupName).SendAsync("ApplicationInfo", SharedCommandArguments.Hub.Arguments.ServiceInfoStart);
+                } else if (!string.IsNullOrWhiteSpace(_remoteResourceSettings.RediscoveryDesktopHubPath))
                 {
                     if (System.IO.File.Exists(_remoteResourceSettings.RediscoveryDesktopHubPath))
                     {
