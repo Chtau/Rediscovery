e0bea5f8daa673b248b505f9d34bc1ef82eb8d9e
Christoph Taucher <taucher.ch@gmail.com>
2020-05-03 14:46:53 +0200

    add repeat for JSON store

--- Rediscovery/Rediscovery/Services/JSONStore.cs
+++ Rediscovery/Rediscovery/Services/JSONStore.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 [assembly: Xamarin.Forms.Dependency(typeof(Rediscovery.Services.JSONStore))]
 namespace Rediscovery.Services
@@ -9,29 +12,79 @@ namespace Rediscovery.Services
     {
         public T GetFileContent<T>(string filePath)
         {
+            return OnGetFileContent<T>(filePath, true).Item2;
+        }
+
+        private Tuple<bool, T> OnGetFileContent<T>(string filePath, bool repeat = false)
+        {
             try
             {
                 if (System.IO.File.Exists(filePath))
                 {
                     var content = System.IO.File.ReadAllText(filePath);
                     if (!string.IsNullOrWhiteSpace(content))
-                        return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(content);
+                        return new Tuple<bool, T>(true, Newtonsoft.Json.JsonConvert.DeserializeObject<T>(content));
                 }
-                return default;
+                return new Tuple<bool, T>(false, default);
+            }
+            catch (System.IO.IOException ioEX)
+            {
+                Tuple<bool, T> result = new Tuple<bool, T>(false, default);
+                if (repeat)
+                {
+                    int count = 0;
+                    do
+                    {
+                        Thread.Sleep(100);
+                        result = OnGetFileContent<T>(filePath);
+                        count++;
+                    } while (count < 5 || result.Item1);
+                }
+                if (!result.Item1)
+                {
+                    _logger.Error(ioEX);
+                    return new Tuple<bool, T>(false, default);
+                }
+                return result;
             }
             catch (Exception ex)
             {
                 _logger.Error(ex);
-                return default;
+                return new Tuple<bool, T>(false, default);
             }
         }
 
         public bool SetFileContent<T>(T value, string filePath)
         {
+            return OnSetFileContent(value, filePath);
+        }
+
+        private bool OnSetFileContent<T>(T value, string filePath, bool repeat = false)
+        {
             try
             {
                 var content = Newtonsoft.Json.JsonConvert.SerializeObject(value);
                 System.IO.File.WriteAllText(filePath, content);
+                return true;
+            }
+            catch (System.IO.IOException ioEX)
+            {
+                bool result = false;
+                if (repeat)
+                {
+                    int count = 0;
+                    do
+                    {
+                        Thread.Sleep(100);
+                        result = SetFileContent(value, filePath);
+                        count++;
+                    } while (count < 5 || result);
+                }
+                if (!result)
+                {
+                    _logger.Error(ioEX);
+                    return false;
+                }
                 return true;
             }
             catch (Exception ex)
