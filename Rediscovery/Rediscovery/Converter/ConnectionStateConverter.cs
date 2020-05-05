5abb1bc9b12fe7268d2e6d0d6ba963b675ddbc9c
Christoph Taucher <taucher.ch@gmail.com>
2020-05-05 11:14:29 +0200

    UI desktop configuration view

---
+++ Rediscovery/Rediscovery/Converter/ConnectionStateConverter.cs
@@ -0,0 +1,61 @@
+﻿using Rediscovery.Services;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Rediscovery.Converter
+{
+    public class ConnectionStateConverter : IValueConverter
+    {
+        internal ILogger _logger => DependencyService.Get<ILogger>() ?? new Logger();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var valueState = (SharedCoreModels.Enums.ConnectionState)(int)value;
+                string resultText = "";
+                switch (valueState)
+                {
+                    case SharedCoreModels.Enums.ConnectionState.None:
+                        resultText = "";
+                        break;
+                    case SharedCoreModels.Enums.ConnectionState.OK:
+                        resultText = "Connected";
+                        break;
+                    case SharedCoreModels.Enums.ConnectionState.Error:
+                        resultText = "Connection error";
+                        break;
+                    case SharedCoreModels.Enums.ConnectionState.Warning:
+                        resultText = "Connection warning";
+                        break;
+                    case SharedCoreModels.Enums.ConnectionState.Offline:
+                        resultText = "Offline";
+                        break;
+                    case SharedCoreModels.Enums.ConnectionState.Denied:
+                        resultText = "Access denied";
+                        break;
+                    case SharedCoreModels.Enums.ConnectionState.WaitForApprovel:
+                        resultText = "Wait for Approval";
+                        break;
+                    default:
+                        resultText = "";
+                        break;
+                }
+                return resultText;
+            } catch (Exception ex)
+            {
+                _logger.Error(ex);
+                return "";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
