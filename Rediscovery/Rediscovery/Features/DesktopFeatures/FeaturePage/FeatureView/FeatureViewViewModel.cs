29921816e993a63b5a13f130a4154a094f3b2407
Christoph Taucher <taucher.ch@gmail.com>
2020-05-07 09:01:13 +0200

    code cleanup

--- Rediscovery/Rediscovery/Features/DesktopFeatures/FeaturePage/FeatureView/FeatureViewViewModel.cs
+++ Rediscovery/Rediscovery/Features/DesktopFeatures/FeaturePage/FeatureView/FeatureViewViewModel.cs
@@ -12,8 +12,6 @@ namespace Rediscovery.Features.DesktopFeatures.FeaturePage.FeatureView
 {
     public class FeatureViewViewModel : BaseFeatureViewModel
     {
-        private bool processIsRunning = false;
-
         private DesktopFeatures.IFeatureUIService featureUIService => DependencyService.Get<DesktopFeatures.IFeatureUIService>() ?? new DesktopFeatures.FeatureUIService();
 
         public event EventHandler<Tuple<Guid, string>> UIDataReady;
@@ -61,6 +59,7 @@ namespace Rediscovery.Features.DesktopFeatures.FeaturePage.FeatureView
                 IsLoading = false
             };
             ConnectionManifestFeature = connectionManifestFeature;
+            Title = connectionManifestFeature.FeatureDisplayName;
             DesktopConfigId = desktopConfigId;
             featureUIService.SaveUI(DesktopConfigId, ConnectionManifestFeature.FeatureId, (state, directory) =>
             {
@@ -94,22 +93,6 @@ namespace Rediscovery.Features.DesktopFeatures.FeaturePage.FeatureView
                     FeatureSetting = settings;
                 }
             });
-
-            //base.ReceivedData += FeatureViewViewModel_ReceivedData;
-            /*if (connectionManifestFeature.Profiles?.Count > 0)
-            {
-                foreach (var item in connectionManifestFeature.Profiles)
-                {
-                    Profiles.Add(item);
-                }
-                SelectedProfile = connectionManifestFeature.Profiles.First();
-                OnProfileChanged();
-            }
-            else
-            {
-                Title = connectionManifestFeature.FeatureDisplayName;
-            }*/
-            Title = connectionManifestFeature.FeatureDisplayName;
         }
 
         public void Send(object sendModel)
@@ -117,36 +100,10 @@ namespace Rediscovery.Features.DesktopFeatures.FeaturePage.FeatureView
             base.Send(SelectedProfile?.Id, sendModel);
         }
 
-        private void FeatureViewViewModel_ReceivedData(object sender, object e)
-        {
-            /*MediaPlayerStateData stateData = Newtonsoft.Json.JsonConvert.DeserializeObject<MediaPlayerStateData>(e?.ToString());
-            if (stateData != null && SelectedProfile != null && string.Equals(SelectedProfile.Id, stateData.ProfileId, StringComparison.OrdinalIgnoreCase))
-            {
-                if (processIsRunning != stateData.ProcessRunning)
-                {
-                    processIsRunning = stateData.ProcessRunning;
-                    OnChangeCanExecute();
-                }
-                CurrentTitle = stateData.CurrentTitle;
-            }*/
-        }
-
         private void OnProfileChanged()
         {
-            processIsRunning = false;
             CurrentTitle = "";
             Title = SelectedProfile.DisplayName;
-            if (!string.IsNullOrWhiteSpace(SelectedProfile.ProfileData?.ToString()))
-            {
-                /*try
-                {
-                    Commands = System.Text.Json.JsonSerializer.Deserialize<List<CommandTypes>>(SelectedProfile.ProfileData?.ToString());
-                }
-                catch (Exception ex)
-                {
-                    logger.Error(ex);
-                }*/
-            }
         }
     }
 }
