9994d8f5e6f071966789519bb07634053cf3930f
Christoph Taucher <taucher.ch@gmail.com>
2020-05-11 13:01:16 +0200

    resource provider extension

---
+++ CommunicationResourceProvider/CommunicationResourceProviderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunicationResourceProvider
+{
+    public static class CommunicationResourceProviderExtensions
+    {
+        public static IApplicationBuilder AddResourceProvider(this IApplicationBuilder app, string hubPath)
+        {
+            app.UseSignalR(x =>
+            {
+                x.MapHub<RemoteResourceHub>(hubPath);
+            });
+            return app;
+        }
+
+        public static IServiceCollection AddResourceProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<IRemoteResourcesSenderService, RemoteResourcesSenderService>();
+            return services;
+        }
+    }
+}
