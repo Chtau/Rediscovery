f9e0ea304dd21d1c308de1b6aa96bec45fd1b089
Christoph Taucher <taucher.ch@gmail.com>
2020-05-13 11:55:36 +0200

    send pending authentication message after added new device

--- DesktopService/Features/Authentication/Auth.cs
+++ DesktopService/Features/Authentication/Auth.cs
@@ -1,4 +1,5 @@
-﻿using SharedCoreModels;
+﻿using Microsoft.Extensions.Logging;
+using SharedCoreModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -16,57 +17,79 @@ namespace DesktopService.Features.Authentication
             OK
         }
 
+        private readonly ILogger<Auth> _logger;
         private readonly ITokenService _tokenService;
         private readonly DALDesktopService.Repository.IDeviceRepository _deviceRepository;
         private readonly DALDesktopService.Repository.IDevicePendingAuthenticationRepository _devicePendingAuthenticationRepository;
+        private readonly CommunicationResourceProvider.IRemoteResourcesSenderService _remoteResourcesSenderService;
 
-        public Auth(ITokenService tokenService,
+        public Auth(ILoggerFactory loggerFactory, ITokenService tokenService,
             DALDesktopService.Repository.IDeviceRepository deviceRepository,
-            DALDesktopService.Repository.IDevicePendingAuthenticationRepository devicePendingAuthenticationRepository)
+            DALDesktopService.Repository.IDevicePendingAuthenticationRepository devicePendingAuthenticationRepository,
+            CommunicationResourceProvider.IRemoteResourcesSenderService remoteResourcesSenderService)
         {
+            _logger = loggerFactory.CreateLogger<Auth>();
             _tokenService = tokenService;
             _deviceRepository = deviceRepository;
             _devicePendingAuthenticationRepository = devicePendingAuthenticationRepository;
+            _remoteResourcesSenderService = remoteResourcesSenderService;
         }
 
         public async Task<Tuple<LoginState, DALDesktopService.Models.Device>> RequestLogin(WelcomeDeviceMessage welcomeDeviceMessage)
         {
-            var u = await _deviceRepository.GetByDeviceIdentifier(welcomeDeviceMessage.DeviceIdentifier);
-            if (u?.AllowAccess == true)
+            try
             {
-                u.Token = _tokenService.CreateNewToken(u.Id.ToString(), u.DeviceName);
-                u.DeviceType = welcomeDeviceMessage.DeviceType;
-                u.Idiom = welcomeDeviceMessage.Idiom;
-                u.Manufacturer = welcomeDeviceMessage.Manufacturer;
-                u.Model = welcomeDeviceMessage.Model;
-                u.OSVersion = welcomeDeviceMessage.OSVersion;
-                u.Platform = welcomeDeviceMessage.Platform;
-                u = await _deviceRepository.SaveDevice(u);
-                return new Tuple<LoginState, DALDesktopService.Models.Device>(LoginState.OK, u);
+                var u = await _deviceRepository.GetByDeviceIdentifier(welcomeDeviceMessage.DeviceIdentifier);
+                if (u?.AllowAccess == true)
+                {
+                    u.Token = _tokenService.CreateNewToken(u.Id.ToString(), u.DeviceName);
+                    u.DeviceType = welcomeDeviceMessage.DeviceType;
+                    u.Idiom = welcomeDeviceMessage.Idiom;
+                    u.Manufacturer = welcomeDeviceMessage.Manufacturer;
+                    u.Model = welcomeDeviceMessage.Model;
+                    u.OSVersion = welcomeDeviceMessage.OSVersion;
+                    u.Platform = welcomeDeviceMessage.Platform;
+                    u = await _deviceRepository.SaveDevice(u);
+                    return new Tuple<LoginState, DALDesktopService.Models.Device>(LoginState.OK, u);
+                }
+                else
+                {
+                    return new Tuple<LoginState, DALDesktopService.Models.Device>(LoginState.RequiredAuthorizeKey, null);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return new Tuple<LoginState, DALDesktopService.Models.Device>(LoginState.RequiredAuthorizeKey, null);
+                _logger.LogError(ex.ToString());
             }
+            return new Tuple<LoginState, DALDesktopService.Models.Device>(LoginState.Failed, null);
         }
 
         public async Task<bool> AddPendingApprovel(WelcomeDeviceMessage welcomeDeviceMessage)
         {
-            var pendingDevice = await _devicePendingAuthenticationRepository.SaveDevicePendingAuthentication(new DALDesktopService.Models.DevicePendingAuthentication
+            try
             {
-                DeviceIdentifier = welcomeDeviceMessage.DeviceIdentifier,
-                DeviceName = welcomeDeviceMessage.DeviceName,
-                DeviceType = welcomeDeviceMessage.DeviceType,
-                Id = Guid.NewGuid(),
-                Idiom = welcomeDeviceMessage.Idiom,
-                Manufacturer = welcomeDeviceMessage.Manufacturer,
-                Model = welcomeDeviceMessage.Model,
-                OSVersion = welcomeDeviceMessage.OSVersion,
-                Platform = welcomeDeviceMessage.Platform,
-                RequestTime = DateTime.UtcNow,
-            });
-            if (pendingDevice != null)
-                return true;
+                var pendingDevice = await _devicePendingAuthenticationRepository.SaveDevicePendingAuthentication(new DALDesktopService.Models.DevicePendingAuthentication
+                {
+                    DeviceIdentifier = welcomeDeviceMessage.DeviceIdentifier,
+                    DeviceName = welcomeDeviceMessage.DeviceName,
+                    DeviceType = welcomeDeviceMessage.DeviceType,
+                    Id = Guid.NewGuid(),
+                    Idiom = welcomeDeviceMessage.Idiom,
+                    Manufacturer = welcomeDeviceMessage.Manufacturer,
+                    Model = welcomeDeviceMessage.Model,
+                    OSVersion = welcomeDeviceMessage.OSVersion,
+                    Platform = welcomeDeviceMessage.Platform,
+                    RequestTime = DateTime.UtcNow,
+                });
+                if (pendingDevice != null)
+                {
+                    _remoteResourcesSenderService.SendPendingAuthenticationDevices();
+                    return true;
+                }
+            } catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+            }
             return false;
         }
     }
