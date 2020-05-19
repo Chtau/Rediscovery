c25c340ef433472f6039d725cb2db0e42fbdae2c
Christoph Taucher <taucher.ch@gmail.com>
2020-05-19 10:31:23 +0200

    implement auto connect and connect

--- Rediscovery/Rediscovery/Features/Connection/ConnectService.cs
+++ Rediscovery/Rediscovery/Features/Connection/ConnectService.cs
@@ -26,25 +26,7 @@ namespace Rediscovery.Features.Connection
                 if (items?.Any(x => x.AutoConnect) == true)
                 {
                     int index = 0;
-                    var item = items[index];
-                    communicationHub.Authenticate(deviceData.GetWelcomeDeviceMessage(), item.ConvertToCommunicationConfigurationModel(), (config, result) =>
-                    {
-                        if (result)
-                        {
-                            communicationHub.Connect(item.ConvertToCommunicationConfigurationModel(), (conResult, state) =>
-                            {
-                                resultCallback?.Invoke(true, state.ConvertToSharedCoreEnum());
-                            });
-                        }
-                        else
-                        {
-                            // TODO: recursive call to connect to the next item
-                            resultCallback?.Invoke(false, config.State.ConvertToSharedCoreEnum());
-                        }
-                    }, (manifest) =>
-                    {
-                        entityManager.AddManifestData(manifest, item.Id, item.DisplayName);
-                    });
+                    OnTryConnect(items, resultCallback, index);
                 }
                 else
                 {
@@ -59,7 +41,36 @@ namespace Rediscovery.Features.Connection
 
         public void Connect(DesktopConfigurationModel desktopConfigurationModel, Action<bool, SharedCoreModels.Enums.ConnectionState> resultCallback)
         {
-            throw new NotImplementedException();
+            OnTryConnect(new List<DesktopConfigurationModel> { desktopConfigurationModel }, resultCallback);
+        }
+
+        private void OnTryConnect(List<DesktopConfigurationModel> desktopConfigurations, Action<bool, SharedCoreModels.Enums.ConnectionState> resultCallback, int nextIndex = 0)
+        {
+            if (desktopConfigurations != null && desktopConfigurations.Count > nextIndex)
+            {
+                var item = desktopConfigurations[nextIndex];
+                communicationHub.Authenticate(deviceData.GetWelcomeDeviceMessage(), item.ConvertToCommunicationConfigurationModel(), (config, result) =>
+                {
+                    if (result)
+                    {
+                        communicationHub.Connect(item.ConvertToCommunicationConfigurationModel(), (conResult, state) =>
+                        {
+                            resultCallback?.Invoke(conResult, state.ConvertToSharedCoreEnum());
+                        });
+                    }
+                    else
+                    {
+                        nextIndex++;
+                        OnTryConnect(desktopConfigurations, resultCallback, nextIndex);
+                    }
+                }, (manifest) =>
+                {
+                    entityManager.AddManifestData(manifest, item.Id, item.DisplayName);
+                });
+            } else
+            {
+                resultCallback?.Invoke(false, SharedCoreModels.Enums.ConnectionState.Error);
+            }
         }
     }
 }
