5eacadcdfe9ee90a488abce31b3740932cdcd921
Christoph Taucher <taucher.ch@gmail.com>
2020-05-26 11:46:41 +0200

    change profile web element

--- DesktopFeatureMediaPlayer/ProfileUI/customelement.js
+++ DesktopFeatureMediaPlayer/ProfileUI/customelement.js
@@ -1,7 +1,55 @@
-﻿class MyComponent extends HTMLElement {
+﻿class JSONComponent extends HTMLElement {
+
+    model = null;
+
+    static get observedAttributes() {
+        return ['setmodel', 'getmodel'];
+    }
+
+    constructor() {
+        super();
+    }
+
     connectedCallback() {
-        this.innerHTML = `<h1>Hello world</h1>`;
+        this.setContent(null);
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name == 'setmodel') {
+            if (newValue) {
+                var newValueObj = JSON.parse(newValue);
+                this.model = newValueObj;
+                this.setContent(newValueObj);
+            } else {
+                this.setContent(null);
+            }
+        } else if (name == 'getmodel') {
+            this.dispatchEvent(new CustomEvent('modelchanged', {
+                detail: this.model
+            }))
+        }
+    }
+
+    setContent(jsonObj) {
+        if (jsonObj) {
+            this.innerHTML = `
+      <style>
+        .content-wrapper {
+          height: 100%;
+          width: 100%;
+        }
+        pre {
+          height: 100%;
+          width: 100%;
+          margin: 0;
+          padding: 5px;
+        }
+      </style>
+      <div class="content-wrapper">
+      <pre>${JSON.stringify(jsonObj, null, 4)}</pre>
+      </div>`
+        } else {
+            this.innerHTML = `<h1>No content</h1>`;
+        }
     }
 }
-
-customElements.define('my-component', MyComponent);
