27d49d0c69e2376fbe83c148a4a4a099f70f0494
Christoph Taucher <taucher.ch@gmail.com>
2020-05-27 20:23:38 +0200

    implement diagnostics logger

--- SharedModels/Logging/DiagnosticsLoggerProvider.cs
+++ SharedModels/Logging/DiagnosticsLoggerProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
 
 namespace SharedBase.Logging
 {
@@ -24,37 +25,57 @@ namespace SharedBase.Logging
 
         public void LogCritical(string message, params object[] args)
         {
-            throw new NotImplementedException();
+            if (args?.Length > 0)
+                message = string.Format(message, args);
+            Debug.WriteLine(message, "Critical");
+            Debug.Indent();
         }
 
         public void LogDebug(string message, params object[] args)
         {
-            throw new NotImplementedException();
+            if (args?.Length > 0)
+                message = string.Format(message, args);
+            Debug.WriteLine(message, "Debug");
+            Debug.Indent();
         }
 
         public void LogError(Exception exception)
         {
-            throw new NotImplementedException();
+            Debug.WriteLine(exception.ToString(), "Error");
+            Debug.Indent();
         }
 
         public void LogError(Exception exception, string message, params object[] args)
         {
-            throw new NotImplementedException();
+            if (args?.Length > 0)
+                message = string.Format(message, args);
+            Debug.WriteLine(exception.ToString(), "Error");
+            Debug.WriteLine(message, "Error");
+            Debug.Indent();
         }
 
         public void LogInformation(string message, params object[] args)
         {
-            throw new NotImplementedException();
+            if (args?.Length > 0)
+                message = string.Format(message, args);
+            Debug.WriteLine(message, "Information");
+            Debug.Indent();
         }
 
         public void LogTrace(string message, params object[] args)
         {
-            throw new NotImplementedException();
+            if (args?.Length > 0)
+                message = string.Format(message, args);
+            Debug.WriteLine(message, "Trace");
+            Debug.Indent();
         }
 
         public void LogWarning(string message, params object[] args)
         {
-            throw new NotImplementedException();
+            if (args?.Length > 0)
+                message = string.Format(message, args);
+            Debug.WriteLine(message, "Warning");
+            Debug.Indent();
         }
         #endregion
 
