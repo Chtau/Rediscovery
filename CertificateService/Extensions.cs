e7857c8d399535ede44f120d974534e6b8d85c56
Christoph Taucher <taucher.ch@gmail.com>
2020-06-01 14:26:53 +0200

    add library to create Certificate for SSL and PEM extensions for X509Certificate2

---
+++ CertificateService/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CertificateService
+{
+    public static class Extensions
+    {
+        public static string ToPEM(this X509Certificate2 x509Certificate2)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine("-----BEGIN CERTIFICATE-----");
+            builder.AppendLine(Convert.ToBase64String(x509Certificate2.Export(X509ContentType.Cert), Base64FormattingOptions.InsertLineBreaks));
+            builder.AppendLine("-----END CERTIFICATE-----");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ CertificateService/ServerCertificate.cs
@@ -0,0 +1,42 @@
+﻿using CertificateManager;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CertificateService
+{
+    public static class ServerCertificate
+    {
+        public static void Create(string outputDirectory, string dnsNameOrIP, string password = "1234", string certFileName = "development", string friendlyName = null)
+        {
+            var serviceProvider = new ServiceCollection()
+                                .AddCertificateManager()
+                                .BuildServiceProvider();
+
+            var _createCertificatesRsa = serviceProvider.GetService<CreateCertificatesRsa>();
+
+            var devCertificate = _createCertificatesRsa
+                .CreateDevelopmentCertificate(dnsNameOrIP, 10);
+            devCertificate.FriendlyName = "development";
+            if (!string.IsNullOrWhiteSpace(friendlyName))
+                devCertificate.FriendlyName = friendlyName;
+
+            var importExportCertificate = serviceProvider.GetService<ImportExportCertificate>();
+
+            // full pfx with password
+            var rootCertInPfxBtyes = importExportCertificate.ExportRootPfx(password, devCertificate);
+            File.WriteAllBytes(Path.Combine(outputDirectory, $"{certFileName}.pfx"), rootCertInPfxBtyes);
+
+            // private key
+            var exportRsaPrivateKeyPem = importExportCertificate.PemExportRsaPrivateKey(devCertificate);
+            File.WriteAllText(Path.Combine(outputDirectory, $"{certFileName}.key"), exportRsaPrivateKeyPem);
+
+            // public key certificate as pem
+            var exportPublicKeyCertificatePem = importExportCertificate.PemExportPublicKeyCertificate(devCertificate);
+            File.WriteAllText(Path.Combine(outputDirectory, $"{certFileName}.pem"), exportPublicKeyCertificatePem);
+        }
+    }
+}
