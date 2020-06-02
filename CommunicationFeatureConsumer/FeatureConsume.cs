76e7538a2cf6efa38caca2d00c8cb3db605bf5ab
Christoph Taucher <taucher.ch@gmail.com>
2020-06-02 11:17:55 +0200

    use SSL for consumer of grpc

---
+++ CommunicationFeatureConsumer/FeatureConsume.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using GrpcService1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunicationFeatureConsumer
+{
+    public class FeatureConsume
+    {
+        public event EventHandler<string> HelloReplay;
+
+        private Greeter.GreeterClient client;
+
+        public void Connect(string ipAddress, int port, string certificatePEM)
+        {
+            var channelCredentials = new SslCredentials(certificatePEM);
+            Channel channel = new Channel(ipAddress, port, channelCredentials);
+            client = new Greeter.GreeterClient(channel);
+        }
+
+        public void SayHello(string value)
+        {
+            var replay = client.SayHello(new HelloRequest { Name = value });
+            HelloReplay?.Invoke(this, replay.Message);
+        }
+    }
+}
