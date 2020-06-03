3e8981c75f850dbc4d7ddb0194b0cbd1773683ea
Christoph Taucher <taucher.ch@gmail.com>
2020-06-03 22:11:12 +0200

    extend token service

--- CommunicationAuthenticationProvider/Services/ITokenService.cs
+++ CommunicationAuthenticationProvider/Services/ITokenService.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text;
 
 namespace CommunicationAuthenticationProvider.Services
 {
     public interface ITokenService
     {
-        string CreateNewToken(string sid, string name);
+        string CreateNewToken(string sid, string name, DateTime? expireDateTime = null);
+        string CreateNewToken(Claim[] claims, DateTime? expireDateTime = null);
     }
 }
--- CommunicationAuthenticationProvider/Services/TokenService.cs
+++ CommunicationAuthenticationProvider/Services/TokenService.cs
@@ -3,6 +3,7 @@ using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 
@@ -17,22 +18,30 @@ namespace CommunicationAuthenticationProvider.Services
             _logger = loggerFactory.CreateLogger<TokenService>();
         }
 
-        public string CreateNewToken(string sid, string name)
+        public string CreateNewToken(string sid, string name, DateTime? expireDateTime = null)
         {
             return OnCreateToken(new Claim[]
                 {
                     new Claim(ClaimTypes.Sid, sid),
                     new Claim(ClaimTypes.Name, name),
-                });
+                }, expireDateTime ?? DateTime.UtcNow.AddDays(7));
         }
 
-        private string OnCreateToken(Claim[] claims)
+        public string CreateNewToken(Claim[] claims, DateTime? expireDateTime = null)
+        {
+            if (claims?.Count() > 0)
+                return OnCreateToken(claims, expireDateTime ?? DateTime.UtcNow.AddDays(7));
+            else
+                throw new ArgumentNullException("claims", "Jwt Token requires at least one Claim");
+        }
+
+        private string OnCreateToken(Claim[] claims, DateTime expireDateTime)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.UtcNow.AddDays(180),
+                Expires = expireDateTime,
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(CommunicationAuthenticationProviderExtensions.TokenSigningKey), SecurityAlgorithms.HmacSha256Signature)
             };
             var token = tokenHandler.CreateToken(tokenDescriptor);
