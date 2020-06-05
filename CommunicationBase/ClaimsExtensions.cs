be09e36c36e3b356f254a53d13759bb43bab94bc
Christoph Taucher <taucher.ch@gmail.com>
2020-06-05 11:19:45 +0200

    send response based on sid

---
+++ CommunicationBase/ClaimsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+public static class ClaimsExtensions
+{
+    public static string GetSid(this IEnumerable<Claim> claims)
+    {
+        return claims.Where(c => c.Type == ClaimTypes.Sid)
+               .Select(c => c.Value).FirstOrDefault();
+    }
+}
--- CommunicationFeatureProvider/ProtoServices/FeatureExchangeService.cs
+++ CommunicationFeatureProvider/ProtoServices/FeatureExchangeService.cs
@@ -1,51 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using Grpc.Core;
 using Featuredata;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace CommunicationFeatureProvider.ProtoServices
 {
     public class FeatureExchangeService : FeatureExchange.FeatureExchangeBase
     {
-        private IServerStreamWriter<DeviceFeatureData> _responseStream;
-        private ServerCallContext _context;
+        private readonly ILogger<FeatureExchangeService> _logger;
+        private Dictionary<string, IServerStreamWriter<DeviceFeatureData>> responseStreams = new Dictionary<string, IServerStreamWriter<DeviceFeatureData>>();
 
         public event EventHandler<PluginFeature.Models.DeviceFeatureData> ReceivedFeatureData;
 
-        public void SendFeatureData(PluginFeature.Models.DeviceFeatureData deviceFeatureData)
+        public FeatureExchangeService(ILoggerFactory loggerFactory)
         {
-            if (_responseStream != null)
+            _logger = loggerFactory.CreateLogger<FeatureExchangeService>();
+        }
+
+        public void SendFeatureData(string sid, PluginFeature.Models.DeviceFeatureData deviceFeatureData)
+        {
+            if (responseStreams.ContainsKey(sid))
             {
                 Task.Run(async () =>
                 {
-                    await _responseStream.WriteAsync(new DeviceFeatureData
+                    try
                     {
-                        Data = deviceFeatureData.Data,
-                        DeviceId = deviceFeatureData.DeviceId,
-                        FeatureId = deviceFeatureData.FeatureId.ToString(),
-                        ProfileId = deviceFeatureData.ProfileId
-                    });
+                        await responseStreams[sid].WriteAsync(new DeviceFeatureData
+                        {
+                            Data = deviceFeatureData.Data,
+                            DeviceId = deviceFeatureData.DeviceId,
+                            FeatureId = deviceFeatureData.FeatureId.ToString(),
+                            ProfileId = deviceFeatureData.ProfileId
+                        });
+                    } catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "SendFeatureData write to response Stream");
+                    }
                 });
             }
         }
 
+        [Authorize]
         public override async Task ExchangeStream(IAsyncStreamReader<DeviceFeatureData> requestStream, IServerStreamWriter<DeviceFeatureData> responseStream, ServerCallContext context)
         {
-            _context = context;
-            _responseStream = responseStream;
-            var user = _context.GetHttpContext().User;
-
-            var readTask = Task.Run(async () =>
+            string sid = null;
+            try
             {
-                await foreach (var message in requestStream.ReadAllAsync())
-                {
-                    ReceivedFeatureData?.Invoke(this, new PluginFeature.Models.DeviceFeatureData(message.DeviceId, message.FeatureId.SafeGuid(), message.ProfileId, message.Data));
-                }
-            });
+                var user = context.GetHttpContext().User;
+                sid = user.Claims.GetSid();
+                if (responseStreams.ContainsKey(sid))
+                    responseStreams[sid] = responseStream;
+                else
+                    responseStreams.Add(sid, responseStream);
 
-            await readTask;
+                var readTask = Task.Run(async () =>
+                {
+                    await foreach (var message in requestStream.ReadAllAsync())
+                    {
+                        ReceivedFeatureData?.Invoke(this, new PluginFeature.Models.DeviceFeatureData(message.DeviceId, message.FeatureId.SafeGuid(), message.ProfileId, message.Data));
+                    }
+                });
+
+                do
+                {
+                    await Task.Delay(100);
+                } while (!context.CancellationToken.IsCancellationRequested);
+
+                await readTask;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "ExchangeStream");
+            } finally
+            {
+                if (!string.IsNullOrWhiteSpace(sid))
+                {
+                    if (responseStreams.ContainsKey(sid))
+                        responseStreams.Remove(sid);
+                }
+            }
         }
     }
 }
