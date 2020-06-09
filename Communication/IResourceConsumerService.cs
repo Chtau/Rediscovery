9bdf0baa755d879ef0f2a7d22eaa4b672ae5bcf5
Christoph Taucher <taucher.ch@gmail.com>
2020-06-09 12:43:10 +0200

    fix missing interface definitions

--- Communication/IResourceConsumerService.cs
+++ Communication/IResourceConsumerService.cs
@@ -1,11 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace CommunicationResourceConsumer
 {
     public interface IResourceConsumerService
     {
+        event EventHandler<List<SharedCoreModels.DeviceInfo>> ReceiveActiveDevices;
+        event EventHandler<List<SharedCoreModels.DeviceInfo>> ReceivePendingDevices;
+        event EventHandler<List<SharedCoreModels.DeviceInfo>> ReceiveDevices;
+        event EventHandler<List<SharedBase.Device.FeatureDefinitionExtended>> ReceiveFeatures;
+        event EventHandler<SharedCoreModels.DeviceInfo> ReceiveUpdateDevices;
+        event EventHandler<(Guid deviceId, bool result)> ReceiveDeleteDevicesResult;
+        event EventHandler<(Guid deviceId, bool accept)> ReceiveResolvePendingDevicesResult;
+        event EventHandler<(Guid featureId, string profileId, bool result)> ReceiveFeatureDetailProfileDeleteResult;
+        event EventHandler<(PluginFeature.Models.DeviceFeatureProfil profile, bool result)> ReceiveFeatureDetailProfileSave;
+        event EventHandler<(PluginFeature.Models.DeviceFeatureSetting setting, bool result)> ReceiveFeatureDetailSettingSave;
+        event EventHandler<Models.FeatureDetail> ReceiveFeatureDetails;
+
         void Connect(string ipAddress, int port, string certificatePEM);
+        void ListenActiveDevices(string token, CancellationTokenSource cts = null);
+        void ListenPendingDevices(string token, CancellationTokenSource cts = null);
+        void ListenDevices(string token, CancellationTokenSource cts = null);
+        void ListenFeatures(string token, CancellationTokenSource cts = null);
+        void UpdateDevice(string token, SharedCoreModels.DeviceInfo deviceInfo);
+        void DeleteDevice(string token, Guid deviceId);
+        void ResolvePendingDevice(string token, Guid deviceId, bool accept);
+        void FeatureDetailProfileDelete(string token, Guid featureId, string profileId);
+        void FeatureDetailProfileSave(string token, PluginFeature.Models.DeviceFeatureProfil profil);
+        void FeatureDetailSettingSave(string token, PluginFeature.Models.DeviceFeatureSetting setting);
+        void FeatureDetail(string token, PluginFeature.Models.DeviceFeatureSetting setting);
     }
 }
