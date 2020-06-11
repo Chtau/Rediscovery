558d5634082fccf8b5d00bf4d6a707b3823682f3
Christoph Taucher <taucher.ch@gmail.com>
2020-06-11 15:01:33 +0200

    remove TODO

--- DesktopService/Worker.cs
+++ DesktopService/Worker.cs
@@ -83,17 +83,13 @@ namespace DesktopService
             {
                 if (disposing)
                 {
-                    // TODO: dispose managed state (managed objects).
+                    
                 }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
 
                 disposedValue = true;
             }
         }
 
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
         // ~Worker()
         // {
         //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
@@ -105,7 +101,6 @@ namespace DesktopService
         {
             // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
             Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
             // GC.SuppressFinalize(this);
         }
         #endregion
