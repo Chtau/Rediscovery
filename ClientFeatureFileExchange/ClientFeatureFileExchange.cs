eefb140590e6313b50e28e0405b6a2f7fbd72bef
Christoph Taucher <taucher.ch@gmail.com>
2020-07-02 20:35:25 +0200

    add config for client file exchange

--- ClientFeatureFileExchange/ClientFeatureFileExchange.cs
+++ ClientFeatureFileExchange/ClientFeatureFileExchange.cs
@@ -1,4 +1,5 @@
 ﻿using PluginFeature;
+using PluginFeature.Interfaces;
 using PluginFeature.Models;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,18 @@ namespace ClientFeatureFileExchange
 {
     public class ClientFeatureFileExchange : BaseClientFeature
     {
+        public override void Init(string pluginDirectory, IPluginLogger pluginLogger)
+        {
+            base.Init(pluginDirectory, pluginLogger);
+            try
+            {
+                var config = Configuration.GetConfigurations(ConfigurationPath());
+            } catch (Exception ex)
+            {
+                pluginLogger.LogError(ex.ToString());
+            }
+        }
+
         public override PluginFeatureDefinitionClient GetDeviceFeatureInfo()
         {
             return new PluginFeatureDefinitionClient
@@ -36,71 +49,111 @@ namespace ClientFeatureFileExchange
             base.ReceiveData(data);
             if (data != null && IsRegister(data.Entity.DeviceId))
             {
-                if (!string.IsNullOrWhiteSpace(data.Entity.Data?.ToString()))
+                try
                 {
-                    var openWithIntent = Newtonsoft.Json.JsonConvert.DeserializeObject<PluginFeature.Models.ClientResources.OpenWithIntent>(data.Entity.Data);
-                    if (openWithIntent.Content?.Length > 0)
+                    var config = Configuration.GetConfigurations(ConfigurationPath());
+                    if (!string.IsNullOrWhiteSpace(data.Entity.Data?.ToString()))
                     {
-                        string fileTitle = openWithIntent.Title;
-                        if (string.IsNullOrWhiteSpace(fileTitle))
-                            fileTitle = $"{DateTime.Now.ToShortDateString()}_{DateTime.Now.ToShortTimeString()}.tmp";
-                        string file = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), fileTitle);
-                        System.IO.File.WriteAllBytes(file, openWithIntent.Content);
-                        pluginLogger.LogInformation($"Save file to {file}");
-                        ProcessStart(file);
-                    } else if (!string.IsNullOrWhiteSpace(openWithIntent.HtmlContent))
-                    {
-                        string fileTitle = openWithIntent.Title;
-                        if (string.IsNullOrWhiteSpace(fileTitle))
-                            fileTitle = $"{DateTime.Now.ToShortDateString()}_{DateTime.Now.ToShortTimeString()}.html";
-                        string file = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), fileTitle);
-                        System.IO.File.WriteAllText(file, openWithIntent.HtmlContent);
-                        pluginLogger.LogInformation($"Save HTML file to {file}");
-                        ProcessStart(file);
-                    }
-                    else if (!string.IsNullOrWhiteSpace(openWithIntent.TextContent))
-                    {
-                        bool isUri = Uri.IsWellFormedUriString(openWithIntent.TextContent, UriKind.RelativeOrAbsolute);
-                        if (isUri)
+                        var openWithIntent = Newtonsoft.Json.JsonConvert.DeserializeObject<PluginFeature.Models.ClientResources.OpenWithIntent>(data.Entity.Data);
+                        if (openWithIntent.Content?.Length > 0)
                         {
-                            pluginLogger.LogInformation($"Open TextContent as URL {openWithIntent.TextContent}");
-                            ProcessStart(openWithIntent.TextContent);
-                        } else
+                            string file = SaveFileBytes(config.WorkingFolder, openWithIntent.Title, openWithIntent.Content, config.FallbackFileExtensionContent);
+                            pluginLogger.LogInformation($"Save file to {file}");
+                            ProcessStart(config.StartProcessName, file);
+                        }
+                        else if (!string.IsNullOrWhiteSpace(openWithIntent.HtmlContent))
                         {
-                            string fileTitle = openWithIntent.Title;
-                            if (string.IsNullOrWhiteSpace(fileTitle))
-                                fileTitle = $"{DateTime.Now.ToShortDateString()}_{DateTime.Now.ToShortTimeString()}.txt";
-                            string file = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), fileTitle);
-                            System.IO.File.WriteAllText(file, openWithIntent.HtmlContent);
-                            pluginLogger.LogInformation($"Save Text file to {file}");
-                            ProcessStart(file);
+                            string file = SaveFileText(config.WorkingFolder, openWithIntent.Title, openWithIntent.HtmlContent, config.FallbackFileExtensionHtml);
+                            pluginLogger.LogInformation($"Save HTML file to {file}");
+                            ProcessStart(config.StartProcessName, file);
+                        }
+                        else if (!string.IsNullOrWhiteSpace(openWithIntent.TextContent))
+                        {
+                            bool isUri = Uri.IsWellFormedUriString(openWithIntent.TextContent, UriKind.RelativeOrAbsolute);
+                            if (isUri)
+                            {
+                                pluginLogger.LogInformation($"Open TextContent as URL {openWithIntent.TextContent}");
+                                ProcessStart(config.StartProcessName, openWithIntent.TextContent);
+                            }
+                            else
+                            {
+                                string file = SaveFileText(config.WorkingFolder, openWithIntent.Title, openWithIntent.TextContent, config.FallbackFileExtensionText);
+                                pluginLogger.LogInformation($"Save Text file to {file}");
+                                ProcessStart(config.StartProcessName, file);
+                            }
+                        }
+                        else if (!string.IsNullOrWhiteSpace(openWithIntent.Uri))
+                        {
+                            bool isUri = Uri.IsWellFormedUriString(openWithIntent.Uri, UriKind.RelativeOrAbsolute);
+                            if (isUri)
+                            {
+                                pluginLogger.LogInformation($"Open Uri as URL {openWithIntent.Uri}");
+                                ProcessStart(config.StartProcessName, openWithIntent.Uri);
+                            }
+                        }
+                        else
+                        {
+                            pluginLogger.LogError($"[OpenWithIntent] received data message but no valid data was present. {Environment.NewLine}(Model:{data.Entity.Data})");
                         }
                     }
-                    else if (!string.IsNullOrWhiteSpace(openWithIntent.Uri))
-                    {
-                        bool isUri = Uri.IsWellFormedUriString(openWithIntent.Uri, UriKind.RelativeOrAbsolute);
-                        if (isUri)
-                        {
-                            pluginLogger.LogInformation($"Open Uri as URL {openWithIntent.Uri}");
-                            ProcessStart(openWithIntent.Uri);
-                        }
-                    } else
-                    {
-                        pluginLogger.LogError($"[OpenWithIntent] received data message but no valid data was present. {Environment.NewLine}(Model:{data.Entity.Data})");
-                    }
+                }
+                catch (Exception ex)
+                {
+                    pluginLogger.LogError(ex.ToString());
                 }
             }
         }
 
-        private void ProcessStart(string file)
+        private string SaveFileText(string directory, string title, string content, string fallbackExtension)
+        {
+            string file = "";
+            try
+            {
+                string fileTitle = title;
+                if (string.IsNullOrWhiteSpace(fileTitle))
+                    fileTitle = $"{DateTime.Now.ToShortDateString()}_{DateTime.Now.ToShortTimeString()}.{fallbackExtension}";
+                file = System.IO.Path.Combine(directory, fileTitle);
+                System.IO.File.WriteAllText(file, content);
+            }
+            catch (Exception ex)
+            {
+                pluginLogger.LogError(ex, $"Could not write File:{file}");
+            }
+            return file;
+        }
+
+        private string SaveFileBytes(string directory, string title, byte[] content, string fallbackExtension)
+        {
+            string file = "";
+            try
+            {
+                string fileTitle = title;
+                if (string.IsNullOrWhiteSpace(fileTitle))
+                    fileTitle = $"{DateTime.Now.ToShortDateString()}_{DateTime.Now.ToShortTimeString()}.{fallbackExtension}";
+                file = System.IO.Path.Combine(directory, fileTitle);
+                System.IO.File.WriteAllBytes(file, content);
+            }
+            catch (Exception ex)
+            {
+                pluginLogger.LogError(ex, $"Could not write File:{file}");
+            }
+            return file;
+        }
+
+        private void ProcessStart(string startProcess, string file)
         {
             try
             {
-                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo("explorer.exe", file));
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(startProcess, file));
             } catch (Exception ex)
             {
                 pluginLogger.LogError(ex, $"Could not start process for File:{file}");
             }
+        }
+
+        private string ConfigurationPath()
+        {
+            return System.IO.Path.Combine(PluginDirectory, "config.json");
         }
     }
 }
---
+++ ClientFeatureFileExchange/Configuration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientFeatureFileExchange
+{
+    public class Configuration
+    {
+        public string WorkingFolder { get; set; }
+        public string StartProcessName { get; set; }
+        public string FallbackFileExtensionContent { get; set; }
+        public string FallbackFileExtensionHtml { get; set; }
+        public string FallbackFileExtensionText { get; set; }
+
+        public static Configuration GetConfigurations(string path)
+        {
+            if (!System.IO.File.Exists(path))
+            {
+                SaveConfigurations(path, new Configuration
+                {
+                    WorkingFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                    StartProcessName = "explorer.exe",
+                    FallbackFileExtensionContent = "tmp",
+                    FallbackFileExtensionHtml = "html",
+                    FallbackFileExtensionText = "txt"
+                });
+            }
+            return OnLoadConfiguration(path);
+        }
+
+        private static void SaveConfigurations(string path, Configuration profiles)
+        {
+            var jsonProfiles = Newtonsoft.Json.JsonConvert.SerializeObject(profiles);
+            System.IO.File.WriteAllText(path, jsonProfiles);
+        }
+
+        private static Configuration OnLoadConfiguration(string path)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<Configuration>(System.IO.File.ReadAllText(path));
+        }
+    }
+}
