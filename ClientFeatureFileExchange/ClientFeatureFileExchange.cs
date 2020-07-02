0f66f8b4c847eb67a9b401b23a2a05cdb0913e19
Christoph Taucher <taucher.ch@gmail.com>
2020-07-02 20:11:31 +0200

    impl. file exchange functions

--- ClientFeatureFileExchange/ClientFeatureFileExchange.cs
+++ ClientFeatureFileExchange/ClientFeatureFileExchange.cs
@@ -2,6 +2,7 @@
 using PluginFeature.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace ClientFeatureFileExchange
@@ -38,8 +39,67 @@ namespace ClientFeatureFileExchange
                 if (!string.IsNullOrWhiteSpace(data.Entity.Data?.ToString()))
                 {
                     var openWithIntent = Newtonsoft.Json.JsonConvert.DeserializeObject<PluginFeature.Models.ClientResources.OpenWithIntent>(data.Entity.Data);
-                    pluginLogger.LogError("TODO: implement handle received [OpenWithIntent]");
+                    if (openWithIntent.Content?.Length > 0)
+                    {
+                        string fileTitle = openWithIntent.Title;
+                        if (string.IsNullOrWhiteSpace(fileTitle))
+                            fileTitle = $"{DateTime.Now.ToShortDateString()}_{DateTime.Now.ToShortTimeString()}.tmp";
+                        string file = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), fileTitle);
+                        System.IO.File.WriteAllBytes(file, openWithIntent.Content);
+                        pluginLogger.LogInformation($"Save file to {file}");
+                        ProcessStart(file);
+                    } else if (!string.IsNullOrWhiteSpace(openWithIntent.HtmlContent))
+                    {
+                        string fileTitle = openWithIntent.Title;
+                        if (string.IsNullOrWhiteSpace(fileTitle))
+                            fileTitle = $"{DateTime.Now.ToShortDateString()}_{DateTime.Now.ToShortTimeString()}.html";
+                        string file = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), fileTitle);
+                        System.IO.File.WriteAllText(file, openWithIntent.HtmlContent);
+                        pluginLogger.LogInformation($"Save HTML file to {file}");
+                        ProcessStart(file);
+                    }
+                    else if (!string.IsNullOrWhiteSpace(openWithIntent.TextContent))
+                    {
+                        bool isUri = Uri.IsWellFormedUriString(openWithIntent.TextContent, UriKind.RelativeOrAbsolute);
+                        if (isUri)
+                        {
+                            pluginLogger.LogInformation($"Open TextContent as URL {openWithIntent.TextContent}");
+                            ProcessStart(openWithIntent.TextContent);
+                        } else
+                        {
+                            string fileTitle = openWithIntent.Title;
+                            if (string.IsNullOrWhiteSpace(fileTitle))
+                                fileTitle = $"{DateTime.Now.ToShortDateString()}_{DateTime.Now.ToShortTimeString()}.txt";
+                            string file = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), fileTitle);
+                            System.IO.File.WriteAllText(file, openWithIntent.HtmlContent);
+                            pluginLogger.LogInformation($"Save Text file to {file}");
+                            ProcessStart(file);
+                        }
+                    }
+                    else if (!string.IsNullOrWhiteSpace(openWithIntent.Uri))
+                    {
+                        bool isUri = Uri.IsWellFormedUriString(openWithIntent.Uri, UriKind.RelativeOrAbsolute);
+                        if (isUri)
+                        {
+                            pluginLogger.LogInformation($"Open Uri as URL {openWithIntent.Uri}");
+                            ProcessStart(openWithIntent.Uri);
+                        }
+                    } else
+                    {
+                        pluginLogger.LogError($"[OpenWithIntent] received data message but no valid data was present. {Environment.NewLine}(Model:{data.Entity.Data})");
+                    }
                 }
+            }
+        }
+
+        private void ProcessStart(string file)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo("explorer.exe", file));
+            } catch (Exception ex)
+            {
+                pluginLogger.LogError(ex, $"Could not start process for File:{file}");
             }
         }
     }
