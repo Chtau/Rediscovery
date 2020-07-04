665659ecdee6f1bcd32a38a4a06edf4d135a0c51
Christoph Taucher <taucher.ch@gmail.com>
2020-07-04 13:56:12 +0200

    add splash screen

--- Rediscovery/Rediscovery.Android/MainActivity.cs
+++ Rediscovery/Rediscovery.Android/MainActivity.cs
@@ -10,8 +10,7 @@ using Android.OS;
 
 namespace Rediscovery.Droid
 {
-    [Activity(Label = "Rediscovery", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    [IntentFilter(new[] { Intent.ActionSend }, Categories = new[] { Intent.CategoryDefault }, DataMimeType = @"*/*")]
+    [Activity(Label = "Rediscovery", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ Rediscovery/Rediscovery.Android/SplashActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Rediscovery.Droid
+{
+    [Activity(Label = "Rediscovery", Icon = "@mipmap/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    [IntentFilter(new[] { Intent.ActionSend }, Categories = new[] { Intent.CategoryDefault }, DataMimeType = @"*/*")]
+    public class SplashActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { OnStartup(); });
+            startupWork.Start();
+        }
+
+        // Prevent the back button from canceling the startup process
+        public override void OnBackPressed() { }
+
+        async void OnStartup()
+        {
+            /*if (Intent.Action == Intent.ActionSend)
+            {
+                StartActivity(new Intent(Intent, typeof(MainActivity)));
+                //StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            } else
+            {
+                StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            }*/
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
