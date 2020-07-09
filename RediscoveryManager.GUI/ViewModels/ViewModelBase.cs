0cf5aee57808624f9a3182ac03fda5f16fac46dc
Christoph Taucher <taucher.ch@gmail.com>
2020-07-09 20:39:27 +0200

    add new project for Manager GUI

---
+++ RediscoveryManager.GUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace RediscoveryManager.GUI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
