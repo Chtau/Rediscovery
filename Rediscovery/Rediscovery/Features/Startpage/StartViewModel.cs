0dcd8d790f188cf589b5e84cf1b24ee4bc7c1205
Christoph Taucher <taucher.ch@gmail.com>
2020-07-20 18:37:43 +0200

    add quick connect

--- Rediscovery/Rediscovery/Features/Startpage/StartViewModel.cs
+++ Rediscovery/Rediscovery/Features/Startpage/StartViewModel.cs
@@ -22,5 +22,12 @@ namespace Rediscovery.Features.Startpage
         // TODO: Show quick connect menu (last connected or near know network or if we have only one configuration)
         // if we are connect show to which configuration we are connected
         // if we have no configuration inform the user that we must add a configuration
+
+        bool isConnect;
+        public bool IsConnect
+        {
+            get { return isConnect; }
+            set { SetProperty(ref isConnect, value); }
+        }
     }
 }
