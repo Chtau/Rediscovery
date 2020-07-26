ac5171b1a4ca3773c3ffbf14f91347e431ac1cb6
Christoph Taucher <taucher.ch@gmail.com>
2020-07-26 10:47:21 +0200

    impl. to handle zip archives

--- DesktopService/Features/Plugins/LoadPlugins.cs
+++ DesktopService/Features/Plugins/LoadPlugins.cs
@@ -4,6 +4,7 @@ using PluginFeature.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -12,6 +13,12 @@ namespace DesktopService.Features.Plugins
 {
     public class LoadPlugins : ILoadPlugins
     {
+        public class PluginAssembly
+        {
+            public Assembly Assembly { get; set; }
+            public string PluginPath { get; set; }
+        }
+
         private readonly IPluginLogger _pluginLogger;
         private readonly ILogger<LoadPlugins> _logger;
         private readonly SharedConfigurations.DesktopService.Models.AppConfiguration _appSettings;
@@ -19,6 +26,7 @@ namespace DesktopService.Features.Plugins
         private List<string> filePaths = new List<string>();
         private List<string> directoryPaths = new List<string>();
         private List<string> missingPluginImplementation = new List<string>();
+        private List<PluginAssembly> pluginAssemblies = new List<PluginAssembly>();
 
         public LoadPlugins(ILoggerFactory loggerFactory, IPluginLogger pluginLogger,
             IOptions<SharedConfigurations.DesktopService.Models.AppConfiguration> appOptions)
@@ -51,17 +59,40 @@ namespace DesktopService.Features.Plugins
                 }
 
                 string defaultPluginDirectory = "plugins";
-                if (Directory.Exists(defaultPluginDirectory))
+                if (!Directory.Exists(defaultPluginDirectory))
                 {
-                    // sub directories are plugins
-                    var subDirs = Directory.EnumerateDirectories(defaultPluginDirectory);
-                    if (subDirs?.Count() > 0)
-                        directoryPaths.AddRange(subDirs);
-                    // zip files in default plugin directory are plugins
-                    var files = Directory.EnumerateFiles(defaultPluginDirectory, "*.zip");
-                    if (files?.Count() > 0)
-                        filePaths.AddRange(files);
+                    Directory.CreateDirectory(defaultPluginDirectory);
                 }
+
+                // zip files in default plugin directory are plugins
+                var files = Directory.EnumerateFiles(defaultPluginDirectory, "*.zip");
+                if (files?.Count() > 0)
+                    filePaths.AddRange(files);
+
+                foreach (var path in filePaths.Where(x => x.EndsWith(".zip")))
+                {
+                    try
+                    {
+                        using (ZipArchive archive = ZipFile.Open(path, ZipArchiveMode.Update))
+                        {
+                            archive.ExtractToDirectory(defaultPluginDirectory, true);
+                        }
+                        File.Delete(path);
+                    } catch (Exception ex)
+                    {
+                        _logger.LogError(ex.ToString());
+                    }
+                }
+                filePaths = filePaths.Where(x => !x.EndsWith(".zip")).ToList();
+
+                // sub directories are plugins
+                var subDirs = Directory.EnumerateDirectories(defaultPluginDirectory);
+                if (subDirs?.Count() > 0)
+                    directoryPaths.AddRange(subDirs);
+
+                // search for plugin assemblies in the directories
+
+                LoadAssemblies();
             } catch (Exception ex)
             {
                 _logger.LogError(ex.ToString());
@@ -70,45 +101,55 @@ namespace DesktopService.Features.Plugins
 
         public IEnumerable<IDeviceFeatureImplementation> GetDeviceFeatureImplementations()
         {
-            return filePaths.SelectMany(pluginPath =>
+            return pluginAssemblies.SelectMany(pluginAssembly =>
             {
-                Assembly pluginAssembly = LoadPlugin(pluginPath);
-                if (pluginAssembly != null)
+                var result = CreateDesktopPluginFeature(pluginAssembly.Assembly, Path.GetDirectoryName(pluginAssembly.PluginPath));
+                if (!(result?.Count() > 0))
                 {
-                    var result = CreateDesktopPluginFeature(pluginAssembly, Path.GetDirectoryName(pluginPath));
-                    if (!(result?.Count() > 0))
-                    {
-                        missingPluginImplementation.Add(pluginPath);
-                    }
-                    return result;
+                    missingPluginImplementation.Add(pluginAssembly.PluginPath);
                 }
-                else
-                    return new List<IDeviceFeatureImplementation>();
+                return result;
             })?.ToList()?.Where(x => x != null);
         }
 
         public IEnumerable<IClientFeatureImplementation> GetClientFeatureImplementations()
         {
-            return filePaths.SelectMany(pluginPath =>
+            return pluginAssemblies.SelectMany(pluginAssembly =>
             {
-                Assembly pluginAssembly = LoadPlugin(pluginPath);
-                if (pluginAssembly != null)
+                var result = CreateClientPluginFeature(pluginAssembly.Assembly, Path.GetDirectoryName(pluginAssembly.PluginPath));
+                if (!(result?.Count() > 0))
                 {
-                    var result = CreateClientPluginFeature(pluginAssembly, Path.GetDirectoryName(pluginPath));
-                    if (!(result?.Count() > 0))
-                    {
-                        missingPluginImplementation.Add(pluginPath);
-                    }
-                    return result;
+                    missingPluginImplementation.Add(pluginAssembly.PluginPath);
                 }
-                else
-                    return new List<IClientFeatureImplementation>();
+                return result;
             })?.ToList()?.Where(x => x != null);
         }
 
         public IEnumerable<string> GetMissingFeatureImplementationsInFilePaths()
         {
             return filePaths.Except(missingPluginImplementation);
+        }
+
+        private void LoadAssemblies()
+        {
+            foreach (var pluginPath in filePaths)
+            {
+                try
+                {
+                    Assembly pluginAssembly = LoadPlugin(pluginPath);
+                    if (pluginAssembly != null)
+                    {
+                        pluginAssemblies.Add(new PluginAssembly
+                        {
+                            Assembly = pluginAssembly,
+                            PluginPath = pluginPath
+                        });
+                    }
+                } catch (Exception ex)
+                {
+                    _logger.LogError(ex.ToString());
+                }
+            }
         }
 
         private Assembly LoadPlugin(string path)
@@ -118,13 +159,13 @@ namespace DesktopService.Features.Plugins
                 PluginLoadContext loadContext = new PluginLoadContext(path);
                 var result = loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(path)));
                 if (result != null)
-                    _logger.LogInformation($"Plugin successful loaded (Assembly:\"{result.GetName().Name}\" Path:\"{path}\")");
+                    _logger.LogInformation($"Assembly successful loaded (Assembly:\"{result.GetName().Name}\" Path:\"{path}\")");
                 else
-                    _logger.LogCritical($"Failed to load Plugin (Path:\"{path}\")");
+                    _logger.LogCritical($"Failed to load Assembly (Path:\"{path}\")");
                 return result;
             } else
             {
-                _logger.LogWarning($"Request Plugin on Configuration Path:\"{path}\" does not exist.");
+                _logger.LogWarning($"Request Assembly on Configuration Path:\"{path}\" does not exist.");
             }
             return null;
         }
