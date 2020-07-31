74931248d4ab160b006f3bd9cc8c4cb5ca9f8264
Christoph Taucher <taucher.ch@gmail.com>
2020-07-31 21:13:17 +0200

    add pipe exchange

---
+++ IPCPipe/IPipeExchange.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCPipe
+{
+    public interface IPipeExchange
+    {
+        void Init(string hub);
+        event EventHandler<string> DataReceived;
+        void Send(string data);
+    }
+}
---
+++ IPCPipe/PipeExchange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCPipe
+{
+    public class PipeExchange : IPipeExchange
+    {
+        public event EventHandler<string> DataReceived;
+
+        private readonly IPipeClient _pipeClient;
+        private readonly IPipeServer _pipeServer;
+
+        private string hub;
+
+        public PipeExchange()
+        {
+            _pipeClient = new IPCPipe.PipeClient();
+            _pipeServer = new IPCPipe.PipeServer();
+        }
+
+        public void Init(string hub)
+        {
+            this.hub = hub;
+        }
+
+        public void Send(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
