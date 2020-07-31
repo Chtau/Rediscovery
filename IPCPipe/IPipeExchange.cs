1a68f27777bcd304790445d43073a78b28165555
Christoph Taucher <taucher.ch@gmail.com>
2020-07-31 23:31:39 +0200

    implement pipe exchange

--- IPCPipe/IPipeExchange.cs
+++ IPCPipe/IPipeExchange.cs
@@ -6,7 +6,7 @@ namespace IPCPipe
 {
     public interface IPipeExchange
     {
-        void Init(string hub);
+        void Init(string hub, string hub_sender, string hub_receiver);
         event EventHandler<string> DataReceived;
         void Send(string data);
     }
--- IPCPipe/PipeExchange.cs
+++ IPCPipe/PipeExchange.cs
@@ -12,6 +12,8 @@ namespace IPCPipe
         private readonly IPipeServer _pipeServer;
 
         private string hub;
+        private string hub_sender;
+        private string hub_receiver;
 
         public PipeExchange()
         {
@@ -19,14 +21,18 @@ namespace IPCPipe
             _pipeServer = new IPCPipe.PipeServer();
         }
 
-        public void Init(string hub)
+        public void Init(string hub, string hub_sender, string hub_receiver)
         {
             this.hub = hub;
+            this.hub_sender = hub_sender;
+            this.hub_receiver = hub_receiver;
+            _pipeServer.Listen($"{hub}_{this.hub_receiver}", (data) => DataReceived?.Invoke(this, data));
         }
 
         public void Send(string data)
         {
-            throw new NotImplementedException();
+            _pipeClient.TryConnect($"{hub}_{hub_sender}");
+            _pipeClient.Send($"{hub}_{hub_sender}", data);
         }
     }
 }
