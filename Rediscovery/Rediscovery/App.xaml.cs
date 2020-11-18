ac362909634342a3c72a1ef6cd027ab2d8b4bb8e
Christoph Taucher <taucher.ch@gmail.com>
2020-11-18 21:21:31 +0100

    add TODO

--- Rediscovery/Rediscovery/App.xaml.cs
+++ Rediscovery/Rediscovery/App.xaml.cs
@@ -10,6 +10,8 @@ namespace Rediscovery
 {
     public partial class App : Application
     {
+        // TODO: https://github.com/xamarin/XamarinCommunityToolkit
+
         public static SharedBase.Core.Version ClientVersion = new SharedBase.Core.Version() { Label = null, Major = 0, Minor = 0, Patch = 0 };
         private IConnectService connect => DependencyService.Get<IConnectService>() ?? new ConnectService();
         private Features.DesktopFeatures.IClientFeatureService clientFeatureService => DependencyService.Get<Features.DesktopFeatures.IClientFeatureService>() ?? new Features.DesktopFeatures.ClientFeatureService();
