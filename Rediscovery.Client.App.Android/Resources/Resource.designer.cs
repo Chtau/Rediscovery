ca94b483b1ae7eb563c0e3fc6ff385ec3f3d120f
Christoph Taucher <taucher.ch@gmail.com>
2020-12-05 19:42:20 +0100

    remove unused resources

--- Rediscovery.Client.App.Android/Resources/Resource.designer.cs
+++ Rediscovery.Client.App.Android/Resources/Resource.designer.cs
@@ -5765,20 +5765,17 @@ namespace Rediscovery.Client.App.Android
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7F0D0003
-			public const int icon = 2131558403;
+			// aapt resource value: 0x7F0D0002
+			public const int icon = 2131558402;
 			
 			// aapt resource value: 0x7F0D0000
 			public const int ic_launcher = 2131558400;
 			
 			// aapt resource value: 0x7F0D0001
-			public const int ic_launcher_foreground = 2131558401;
+			public const int ic_launcher_round = 2131558401;
 			
-			// aapt resource value: 0x7F0D0002
-			public const int ic_launcher_round = 2131558402;
-			
-			// aapt resource value: 0x7F0D0004
-			public const int launcher_foreground = 2131558404;
+			// aapt resource value: 0x7F0D0003
+			public const int launcher_foreground = 2131558403;
 			
 			static Mipmap()
 			{
