098362f1712846335fc3c415c7b6aa06684cf955
Christoph Taucher <taucher.ch@gmail.com>
2020-12-09 20:19:39 +0100

    migration to android x

--- Rediscovery.Client.App.MobileAndroid/MainActivity.cs
+++ Rediscovery.Client.App.MobileAndroid/MainActivity.cs
@@ -3,11 +3,14 @@ using Android;
 using Android.App;
 using Android.OS;
 using Android.Runtime;
-using Android.Support.Design.Widget;
-using Android.Support.V4.View;
-using Android.Support.V4.Widget;
-using Android.Support.V7.App;
 using Android.Views;
+using AndroidX.AppCompat.App;
+using AndroidX.AppCompat.Widget;
+using AndroidX.Core.View;
+using AndroidX.DrawerLayout.Widget;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Navigation;
+using Google.Android.Material.Snackbar;
 
 namespace Rediscovery.Client.App.MobileAndroid
 {
@@ -19,7 +22,7 @@ namespace Rediscovery.Client.App.MobileAndroid
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
