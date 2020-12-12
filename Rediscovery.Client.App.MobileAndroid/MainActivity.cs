435028d3772b567eb17dfc26c665d4174c16b29c
Christoph Taucher <taucher.ch@gmail.com>
2020-12-12 17:13:25 +0100

    add splash screen

--- Rediscovery.Client.App.MobileAndroid/MainActivity.cs
+++ Rediscovery.Client.App.MobileAndroid/MainActivity.cs
@@ -14,7 +14,7 @@ using Google.Android.Material.Snackbar;
 
 namespace Rediscovery.Client.App.MobileAndroid
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Icon = "@mipmap/ic_launcher", Theme = "@style/AppTheme.NoActionBar", MainLauncher = false)]
     public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
     {
         protected override void OnCreate(Bundle savedInstanceState)
