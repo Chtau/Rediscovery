fb26640c9856d16e7406280f38b65ee2ba7cfd41
Christoph Taucher <taucher.ch@gmail.com>
2020-12-13 13:28:58 +0100

    update resources

--- Rediscovery.Client.App.MobileAndroid/Resources/Resource.designer.cs
+++ Rediscovery.Client.App.MobileAndroid/Resources/Resource.designer.cs
@@ -6011,9 +6011,6 @@ namespace Rediscovery.Client.App.MobileAndroid
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7F0D0003
-			public const int icon = 2131558403;
-			
 			// aapt resource value: 0x7F0D0000
 			public const int ic_launcher = 2131558400;
 			
@@ -6022,9 +6019,6 @@ namespace Rediscovery.Client.App.MobileAndroid
 			
 			// aapt resource value: 0x7F0D0002
 			public const int ic_launcher_round = 2131558402;
-			
-			// aapt resource value: 0x7F0D0004
-			public const int launcher_foreground = 2131558404;
 			
 			static Mipmap()
 			{
