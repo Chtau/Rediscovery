5a5c0dbe47f4902bba5492d9cb10160ab408af81
Christoph Taucher <taucher.ch@gmail.com>
2020-12-22 21:07:33 +0100

    dev. devices service

--- Rediscovery.Client.App.Core/Features/Connect/DevicesService.cs
+++ Rediscovery.Client.App.Core/Features/Connect/DevicesService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rediscovery.Client.App.Core.Features.Connect.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,28 @@ namespace Rediscovery.Client.App.Core.Features.Connect
 {
     public class DevicesService : IDevicesService
     {
+        private List<IConnectDevice> connectDevices = new List<IConnectDevice>();
+
+        public event EventHandler<DeviceConnectionState> ConnectionStateChanged;
+
+        public void Autoconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Connect(Guid connectionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Disconnect(Guid connectionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Probe(Guid connectionId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Rediscovery.Client.App.Core/Features/Connect/IDevicesService.cs
+++ Rediscovery.Client.App.Core/Features/Connect/IDevicesService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rediscovery.Client.App.Core.Features.Connect.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,10 @@ namespace Rediscovery.Client.App.Core.Features.Connect
 {
     public interface IDevicesService
     {
+        event EventHandler<DeviceConnectionState> ConnectionStateChanged;
+        bool Probe(Guid connectionId);
+        void Connect(Guid connectionId);
+        bool Disconnect(Guid connectionId);
+        void Autoconnect();
     }
 }
