989984d9e82cffc81ceea2bfe361ae197fb3663d
Christoph Taucher <taucher.ch@gmail.com>
2020-12-25 13:07:20 +0100

    add unit test library for core library

---
+++ Rediscovery.Client.App.Core.Test/CoreManagerTest.cs
@@ -0,0 +1,35 @@
+﻿using Rediscovery.Client.App.Core.Features.Discovery;
+using Rediscovery.Client.App.Core.Features.Storage;
+using Rediscovery.Client.App.Core.Resources;
+using Rediscovery.Shared.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Rediscovery.Client.App.Core
+{
+    public class CoreManagerTest
+    {
+        [Fact]
+        public void Init()
+        {
+            CoreManager.Init(new CoreManagerSetting
+            {
+                CurrentDiscoverSetting = new Features.Discovery.DiscoverSetting
+                {
+                    Port = 14545
+                },
+                CurrentStorageSetting = new Features.Storage.StorageSetting
+                {
+                    DatabaseFile = null
+                }
+            });
+            Assert.NotNull(Dependency.Resolver.Get<ILogger>());
+            Assert.NotNull(Dependency.Resolver.Get<IDBStorage>());
+            Assert.NotNull(Dependency.Resolver.Get<IJSONStorage>());
+            Assert.NotNull(Dependency.Resolver.Get<IAssemblyResourceProvider>());
+            Assert.NotNull(Dependency.Resolver.Get<IDiscoverDevices>());
+        }
+    }
+}
