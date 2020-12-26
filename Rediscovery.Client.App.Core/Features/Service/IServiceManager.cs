7dda3fe4b4c5d6856f3af86e72d88d57a7b6932b
Christoph Taucher <taucher.ch@gmail.com>
2020-12-26 21:28:46 +0100

    add service manager

---
+++ Rediscovery.Client.App.Core/Features/Service/IServiceManager.cs
@@ -0,0 +1,13 @@
+﻿using Rediscovery.Client.App.Core.Features.Device.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rediscovery.Client.App.Core.Features.Service
+{
+    public interface IServiceManager
+    {
+        event EventHandler<object> DeviceConnectionStateChanged;
+        event EventHandler<HeartbeatResult> HeartbeatReceived;
+    }
+}
---
+++ Rediscovery.Client.App.Core/Features/Service/ServiceManager.cs
@@ -0,0 +1,18 @@
+﻿using Rediscovery.Client.App.Core.Features.Device.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rediscovery.Client.App.Core.Features.Service
+{
+    public class ServiceManager : IServiceManager
+    {
+        public event EventHandler<object> DeviceConnectionStateChanged;
+        public event EventHandler<HeartbeatResult> HeartbeatReceived;
+
+        public ServiceManager()
+        {
+            // TODO: initialize all provider services
+        }
+    }
+}
