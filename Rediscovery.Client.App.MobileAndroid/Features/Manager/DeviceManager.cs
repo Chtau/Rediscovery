1dc3d26f9d8f19b3e9e5f598dd78356fea953a61
Christoph Taucher <taucher.ch@gmail.com>
2021-03-02 19:32:36 +0100

    fix save device function

--- Rediscovery.Client.App.MobileAndroid/Features/Manager/DeviceManager.cs
+++ Rediscovery.Client.App.MobileAndroid/Features/Manager/DeviceManager.cs
@@ -32,7 +32,7 @@ namespace Rediscovery.Client.App.MobileAndroid.Features.Manager
             try
             {
                 // TODO: remove test data
-                Core.Database.Instance.Reset();
+                // Core.Database.Instance.Reset();
                 Save(OnCreateMockDevice(new Guid("7AE08E8C-2C74-462C-950F-9EF82022A7B3"), "Device1", 1, new Guid("E4E7802E-07D1-4D99-A93C-04C4085F6B5E"), new Guid("C24FACAE-9368-4035-B5B3-5DB28C4A75B1"), new Guid("F24CA915-4297-4A22-AFBD-F4C4992E7FFF"), new Guid("279DD3C2-5E90-4A44-9569-15B719CC0867"), new Guid("6D15B5C4-4DB3-4B14-AD38-5EF0076F670A")));
                 Save(OnCreateMockDevice(new Guid("E6BAAEB3-D92F-4FC1-85FE-92C4ED0F6644"), "Device2", 2, new Guid("5207AA48-99D5-43EA-9E60-439129A8328B"), new Guid("DE2A47C7-62C2-455F-B215-3D039FFB58BC"), new Guid("BC846F35-28BA-4B66-8A55-B187405BC312")));
                 var devFav = OnCreateMockDevice(new Guid("C05A431E-4AD9-4F3B-A083-3B0365484684"), "Device3", 3, new Guid("0E2F4262-3E30-4126-B415-DDACA771BD1E"), new Guid("75482B34-73B5-4183-991B-1380D00CD449"), new Guid("7D663F65-5112-4605-BEB0-FE8C2A997BDB"), new Guid("8AB94500-351B-4364-BEAE-D9D2965CCA44"), new Guid("9D8CB391-4AA3-477F-9F22-17043B7D5D73"), new Guid("BB963E79-8762-4A66-8FC6-1D04B701C940"), new Guid("25C4B1E5-57DF-4E72-9854-86E455984125"));
@@ -65,9 +65,9 @@ namespace Rediscovery.Client.App.MobileAndroid.Features.Manager
                     {
                         if (device.OrderBy <= 0)
                         {
-                            var maxOrder = Core.Database.Instance.GetAll<Features.Models.Device>()?.Max(x => x.OrderBy);
-                            if (maxOrder > 0)
-                                device.OrderBy = maxOrder.Value;
+                            var maxOrder = Core.Database.Instance.GetAll<Features.Models.Device>()?.OrderByDescending(x => x.OrderBy)?.Select(x => x.OrderBy)?.FirstOrDefault() ?? 0;
+                            if (maxOrder >= 0)
+                                device.OrderBy = ++maxOrder;
                         }
                         Core.Database.Instance.Insert(device);
                     }
