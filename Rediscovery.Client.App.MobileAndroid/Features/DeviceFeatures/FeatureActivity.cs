c0b71e2591f9bc6e0fb5eeb3df821a7e0da9e343
Christoph Taucher <taucher.ch@gmail.com>
2021-03-03 22:22:01 +0100

    add fab menu

--- Rediscovery.Client.App.MobileAndroid/Features/DeviceFeatures/FeatureActivity.cs
+++ Rediscovery.Client.App.MobileAndroid/Features/DeviceFeatures/FeatureActivity.cs
@@ -5,6 +5,7 @@ using Android.Runtime;
 using Android.Views;
 using AndroidX.AppCompat.App;
 using AndroidX.AppCompat.Widget;
+using Google.Android.Material.FloatingActionButton;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,10 @@ namespace Rediscovery.Client.App.MobileAndroid.Features.DeviceFeatures
         public const string Key_DeviceId = "deviceid";
 
         public Guid DeviceId { get; private set; } = Guid.Empty;
+        private bool isFabMenuOpen = false;
+        private FloatingActionButton fab1;
+        private FloatingActionButton fab2;
+        private FloatingActionButton fab3;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -31,6 +36,14 @@ namespace Rediscovery.Client.App.MobileAndroid.Features.DeviceFeatures
                 SetContentView(Resource.Layout.feature_detail);
                 /*Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
                 SetSupportActionBar(toolbar);*/
+                var fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+                fab1 = FindViewById<FloatingActionButton>(Resource.Id.fab1);
+                fab2 = FindViewById<FloatingActionButton>(Resource.Id.fab2);
+                fab3 = FindViewById<FloatingActionButton>(Resource.Id.fab3);
+                fab.Click += (obj, args) =>
+                {
+                    OnToogleFabMenu();
+                };
 
                 var deviceIdString = Intent.Extras.GetString(Key_DeviceId);
                 if (!string.IsNullOrWhiteSpace(deviceIdString))
@@ -38,6 +51,31 @@ namespace Rediscovery.Client.App.MobileAndroid.Features.DeviceFeatures
                     DeviceId = new Guid(deviceIdString);
                 }
             } catch (Exception ex)
+            {
+                Core.Logger.Instance.Error(ex);
+            }
+        }
+
+        private void OnToogleFabMenu()
+        {
+            try
+            {
+                if (isFabMenuOpen)
+                {
+                    isFabMenuOpen = !isFabMenuOpen;
+                    fab1.Animate().TranslationY(0);
+                    fab2.Animate().TranslationY(0);
+                    fab3.Animate().TranslationY(0);
+                } else
+                {
+                    isFabMenuOpen = !isFabMenuOpen;
+                    
+                    fab1.Animate().TranslationY(Resources.GetDimension(Resource.Dimension.standard_55));
+                    fab2.Animate().TranslationY(Resources.GetDimension(Resource.Dimension.standard_105));
+                    fab3.Animate().TranslationY(Resources.GetDimension(Resource.Dimension.standard_155));
+                }
+            }
+            catch (Exception ex)
             {
                 Core.Logger.Instance.Error(ex);
             }
