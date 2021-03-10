659022e42b4f1dd833db4bbf0837f1aaba010238
Christoph Taucher <taucher.ch@gmail.com>
2021-03-10 22:25:39 +0100

    add new mock data for remote and local features

--- Rediscovery.Client.App.MobileAndroid/Features/Manager/DeviceManager.cs
+++ Rediscovery.Client.App.MobileAndroid/Features/Manager/DeviceManager.cs
@@ -32,15 +32,27 @@ namespace Rediscovery.Client.App.MobileAndroid.Features.Manager
             try
             {
                 // TODO: remove test data
-                // Core.Database.Instance.Reset();
-                /*
-                Save(OnCreateMockDevice(new Guid("7AE08E8C-2C74-462C-950F-9EF82022A7B3"), "Device1", 1, new Guid("E4E7802E-07D1-4D99-A93C-04C4085F6B5E"), new Guid("C24FACAE-9368-4035-B5B3-5DB28C4A75B1"), new Guid("F24CA915-4297-4A22-AFBD-F4C4992E7FFF"), new Guid("279DD3C2-5E90-4A44-9569-15B719CC0867"), new Guid("6D15B5C4-4DB3-4B14-AD38-5EF0076F670A")), false);
-                Save(OnCreateMockDevice(new Guid("E6BAAEB3-D92F-4FC1-85FE-92C4ED0F6644"), "Device2", 2, new Guid("5207AA48-99D5-43EA-9E60-439129A8328B"), new Guid("DE2A47C7-62C2-455F-B215-3D039FFB58BC"), new Guid("BC846F35-28BA-4B66-8A55-B187405BC312")), false);
-                var devFav = OnCreateMockDevice(new Guid("C05A431E-4AD9-4F3B-A083-3B0365484684"), "Device3", 3, new Guid("0E2F4262-3E30-4126-B415-DDACA771BD1E"), new Guid("75482B34-73B5-4183-991B-1380D00CD449"), new Guid("7D663F65-5112-4605-BEB0-FE8C2A997BDB"), new Guid("8AB94500-351B-4364-BEAE-D9D2965CCA44"), new Guid("9D8CB391-4AA3-477F-9F22-17043B7D5D73"), new Guid("BB963E79-8762-4A66-8FC6-1D04B701C940"), new Guid("25C4B1E5-57DF-4E72-9854-86E455984125"));
+                Core.Database.Instance.Reset();
+                
+                Save(OnCreateMockDevice(new Guid("7AE08E8C-2C74-462C-950F-9EF82022A7B3"), "Device1", 1, 
+                    OnCreateMockFeatures(false, new Guid("E4E7802E-07D1-4D99-A93C-04C4085F6B5E"), new Guid("C24FACAE-9368-4035-B5B3-5DB28C4A75B1"), new Guid("F24CA915-4297-4A22-AFBD-F4C4992E7FFF"), new Guid("279DD3C2-5E90-4A44-9569-15B719CC0867"), new Guid("6D15B5C4-4DB3-4B14-AD38-5EF0076F670A")),
+                    OnCreateMockFeatures(true, new Guid("2A1AF89A-E293-4113-82A1-C63C628081C4"), new Guid("801A03E5-AEC9-4DDD-9193-1D4F8B8CAFB9"), new Guid("6A83B532-F5E3-4653-87FF-7254E2D27366"))
+                    ), false);
+                Save(OnCreateMockDevice(new Guid("E6BAAEB3-D92F-4FC1-85FE-92C4ED0F6644"), "Device2", 2,
+                    OnCreateMockFeatures(false, new Guid("5207AA48-99D5-43EA-9E60-439129A8328B"), new Guid("DE2A47C7-62C2-455F-B215-3D039FFB58BC"), new Guid("BC846F35-28BA-4B66-8A55-B187405BC312")),
+                    OnCreateMockFeatures(true, new Guid("2A1AF89A-E293-4113-82A1-C63C628081C4"), new Guid("801A03E5-AEC9-4DDD-9193-1D4F8B8CAFB9"), new Guid("6A83B532-F5E3-4653-87FF-7254E2D27366"))
+                    ), false);
+                var devFav = OnCreateMockDevice(new Guid("C05A431E-4AD9-4F3B-A083-3B0365484684"), "Device3", 3,
+                    OnCreateMockFeatures(false, new Guid("0E2F4262-3E30-4126-B415-DDACA771BD1E"), new Guid("75482B34-73B5-4183-991B-1380D00CD449"), new Guid("7D663F65-5112-4605-BEB0-FE8C2A997BDB"), new Guid("8AB94500-351B-4364-BEAE-D9D2965CCA44"), new Guid("9D8CB391-4AA3-477F-9F22-17043B7D5D73"), new Guid("BB963E79-8762-4A66-8FC6-1D04B701C940"), new Guid("25C4B1E5-57DF-4E72-9854-86E455984125")),
+                    OnCreateMockFeatures(true, new Guid("2A1AF89A-E293-4113-82A1-C63C628081C4"), new Guid("801A03E5-AEC9-4DDD-9193-1D4F8B8CAFB9"), new Guid("6A83B532-F5E3-4653-87FF-7254E2D27366"))
+                    );
                 devFav.IsFavorite = true;
                 Save(devFav, false);
-                Save(OnCreateMockDevice(new Guid("8E26DEEE-1691-43A5-8752-F641D0AACCA0"), "Device4", 4, new Guid("85A450C4-DE90-420D-944B-A0E667FAD3C4"), new Guid("867BAB40-61A3-403B-BDFF-A3F958967BC4"), new Guid("DAA3E66F-2900-4B39-BC99-BB6F67CC9E67"), new Guid("D1706F5F-1A3C-49B4-8698-A954F9853F92")), false);
-                */
+                Save(OnCreateMockDevice(new Guid("8E26DEEE-1691-43A5-8752-F641D0AACCA0"), "Device4", 4,
+                    OnCreateMockFeatures(false, new Guid("85A450C4-DE90-420D-944B-A0E667FAD3C4"), new Guid("867BAB40-61A3-403B-BDFF-A3F958967BC4"), new Guid("DAA3E66F-2900-4B39-BC99-BB6F67CC9E67"), new Guid("D1706F5F-1A3C-49B4-8698-A954F9853F92")),
+                    OnCreateMockFeatures(true, new Guid("2A1AF89A-E293-4113-82A1-C63C628081C4"), new Guid("801A03E5-AEC9-4DDD-9193-1D4F8B8CAFB9"), new Guid("6A83B532-F5E3-4653-87FF-7254E2D27366"))
+                    ), false);
+                
                 OnDeviceUpdateOrderBy();
             }
             catch (Exception ex)
@@ -235,7 +247,7 @@ namespace Rediscovery.Client.App.MobileAndroid.Features.Manager
             }
         }
 
-        private Features.Models.Device OnCreateMockDevice(Guid deviceId, string name, int viewId, params Guid[] features)
+        private Features.Models.Device OnCreateMockDevice(Guid deviceId, string name, int viewId, List<Features.Models.Feature> remoteFeatures, List<Features.Models.Feature> localFeatures)
         {
             var device = new Features.Models.Device
             {
@@ -244,19 +256,27 @@ namespace Rediscovery.Client.App.MobileAndroid.Features.Manager
                 OrderBy = 0,
                 ViewId = viewId
             };
+            device.FeaturesRemote = remoteFeatures;
+            device.FeaturesLocal = localFeatures;
+            return device;
+        }
+
+        private List<Features.Models.Feature> OnCreateMockFeatures(bool isLocal, params Guid[] features)
+        {
+            var retVal = new List<Features.Models.Feature>();
             if (features?.Length > 0)
             {
-                device.FeaturesRemote = new System.Collections.Generic.List<Features.Models.Feature>();
                 foreach (var item in features)
                 {
-                    device.FeaturesRemote.Add(new Features.Models.Feature
+                    retVal.Add(new Features.Models.Feature
                     {
                         FeatureId = item,
-                        Name = item.ToString()
+                        Name = item.ToString(),
+                        IsLocal = isLocal
                     });
                 }
             }
-            return device;
+            return retVal;
         }
     }
 }
