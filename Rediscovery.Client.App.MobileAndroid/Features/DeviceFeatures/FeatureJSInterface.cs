c4a564530764d0ca45948f0cb212c4478b176614
Christoph Taucher <taucher.ch@gmail.com>
2021-04-25 18:46:18 +0200

    add features function to js interface

--- Rediscovery.Client.App.MobileAndroid/Features/DeviceFeatures/FeatureJSInterface.cs
+++ Rediscovery.Client.App.MobileAndroid/Features/DeviceFeatures/FeatureJSInterface.cs
@@ -15,11 +15,21 @@ namespace Rediscovery.Client.App.MobileAndroid.Features.DeviceFeatures
 {
     public class FeatureJSInterface : Java.Lang.Object
     {
-        Context context;
+        private readonly Context context;
+        private Action<string> actionCallback;
+        private Action domReadyCallback;
+        private Action<string> loggerCallback;
 
         public FeatureJSInterface(Context context)
         {
             this.context = context;
+        }
+
+        public void RegisterListener(Action<string> actionCallback, Action domReadyCallback, Action<string> loggerCallback)
+        {
+            this.actionCallback = actionCallback;
+            this.domReadyCallback = domReadyCallback;
+            this.loggerCallback = loggerCallback;
         }
 
         [Export]
@@ -27,6 +37,48 @@ namespace Rediscovery.Client.App.MobileAndroid.Features.DeviceFeatures
         public void ShowToast()
         {
             Toast.MakeText(context, "Hello from C#", ToastLength.Short).Show();
+        }
+
+        [Export]
+        [JavascriptInterface]
+        public void InvokeAction(string data)
+        {
+            try
+            {
+                actionCallback?.Invoke(data);
+            }
+            catch (Exception ex)
+            {
+                loggerCallback?.Invoke(ex.ToString());
+            }
+        }
+
+        [Export]
+        [JavascriptInterface]
+        public void InvokeDOMReady()
+        {
+            try
+            {
+                domReadyCallback?.Invoke();
+            }
+            catch (Exception ex)
+            {
+                loggerCallback?.Invoke(ex.ToString());
+            }
+        }
+
+        [Export]
+        [JavascriptInterface]
+        public void InvokeLogger(string data)
+        {
+            try
+            {
+                loggerCallback?.Invoke(data);
+            }
+            catch (Exception ex)
+            {
+                loggerCallback?.Invoke(ex.ToString());
+            }
         }
     }
 }
