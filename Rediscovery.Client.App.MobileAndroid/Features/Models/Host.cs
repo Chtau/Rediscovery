19d57c6e360bf8cefb1ab073b5c153c592c942af
Christoph Taucher <taucher.ch@gmail.com>
2021-05-14 17:17:55 +0200

    add host model

---
+++ Rediscovery.Client.App.MobileAndroid/Features/Models/Host.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rediscovery.Client.App.MobileAndroid.Features.Models
+{
+    public class Host
+    {
+        public Guid HostId { get; set; }
+        public string FriendlyName { get; set; }
+        public string IP { get; set; }
+        public int Port { get; set; }
+        public bool AutoConnect { get; set; }
+        public List<Device> Devices { get; set; }
+    }
+}
