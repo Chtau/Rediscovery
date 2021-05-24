dbcd988b849c1906c8610e0ce9584274a5c31c6f
Christoph Taucher <taucher.ch@gmail.com>
2021-05-24 10:12:07 +0200

    extend setting for send ports

--- Rediscovery.Communication.Protocol/Setting.cs
+++ Rediscovery.Communication.Protocol/Setting.cs
@@ -7,18 +7,24 @@ namespace Rediscovery.Communication.Protocol
     public class Setting
     {
         public const int DefaultListenPortDiscovery = 13570;
+        public const int DefaultSendPortDiscovery = 13570;
         public const int DefaultListenPortData = 13571;
+        public const int DefaultSendPortData = 13571;
         public const int DefaultListenPortLowData = 13572;
+        public const int DefaultSendPortLowData = 13572;
         public const int DefaultPackageBytes = 1024;
 
         public int ListenPortDiscovery { get; set; } = DefaultListenPortDiscovery;
         public int ListenPackageBytesDiscovery { get; set; } = DefaultPackageBytes;
+        public int SendPortDiscovery { get; set; } = DefaultSendPortDiscovery;
         public int SendPackageBytesDiscovery { get; set; } = DefaultPackageBytes;
         public int ListenPortData { get; set; } = DefaultListenPortData;
         public int ListenPackageBytesData { get; set; } = DefaultPackageBytes;
+        public int SendPortData { get; set; } = DefaultSendPortData;
         public int SendPackageBytesData { get; set; } = DefaultPackageBytes;
         public int ListenPortLowData { get; set; } = DefaultListenPortLowData;
         public int ListenPackageBytesLowData { get; set; } = DefaultPackageBytes;
+        public int SendPortLowData { get; set; } = DefaultSendPortLowData;
         public int SendPackageBytesLowData { get; set; } = DefaultPackageBytes;
     }
 }
