1ff37ad98e88fb8042fddcae4ed42e378c518c47
Christoph Taucher <taucher.ch@gmail.com>
2021-06-03 10:20:24 +0200

    add data transfer unit test for long text

--- Rediscovery.Communication.Protocol.Test/SendReceive.cs
+++ Rediscovery.Communication.Protocol.Test/SendReceive.cs
@@ -9,6 +9,7 @@ namespace Rediscovery.Communication.Protocol.Test
         [Fact]
         public void SendReceiveSocketSimpleText()
         {
+            string content = "Test";
             IRediscoveryProtocol protocol1 = Shared.TestDevice(new Models.ConnectionConfiguration(16576, 16577, 1024));
 
             IRediscoveryProtocol protocol = new RediscoveryProtocol();
@@ -30,7 +31,7 @@ namespace Rediscovery.Communication.Protocol.Test
             System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
             Task.Run(() =>
             {
-                protocol.Send(new Transfer<string>(protocol.Devices[0].Identifier, "Test"), (success) =>
+                protocol.Send(new Transfer<string>(protocol.Devices[0].Identifier, content), (success) =>
                 {
                     if (success != TransportState.Ok)
                         throw new Exception("Failed to send");
@@ -41,7 +42,48 @@ namespace Rediscovery.Communication.Protocol.Test
             {
                 System.Threading.Thread.Sleep(TimeSpan.FromMilliseconds(10));
             } while (!stop);
-            Assert.True(data == "Test", "No Data received via Socket");
+            Assert.True(data == content, "No Data received via Socket");
+
+            protocol1.Stop();
+            protocol.Stop();
+        }
+
+        [Fact]
+        public void SendReceiveLongText()
+        {
+            string content = string.Join("", System.Linq.Enumerable.Repeat($"{DateTime.UtcNow:yyyy-MM-dd-HH-mm-FFFFFFF}", 1000));
+            IRediscoveryProtocol protocol1 = Shared.TestDevice(new Models.ConnectionConfiguration(16576, 16577, 1024));
+            IRediscoveryProtocol protocol = new RediscoveryProtocol();
+            protocol.Start(null);
+            Task.Delay(TimeSpan.FromSeconds(1)).GetAwaiter().GetResult();
+
+            bool stop = false;
+            string data = null;
+            Task.Run(async () =>
+            {
+                protocol1.Listen<string>((transfer) =>
+                {
+                    data = transfer.Content;
+                    stop = true;
+                });
+                await Task.Delay(TimeSpan.FromSeconds(5));
+                stop = true;
+            });
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+            Task.Run(() =>
+            {
+                protocol.Send(new Transfer<string>(protocol.Devices[0].Identifier, content), (success) =>
+                {
+                    if (success != TransportState.Ok)
+                        throw new Exception("Failed to send");
+                });
+            });
+
+            do
+            {
+                System.Threading.Thread.Sleep(TimeSpan.FromMilliseconds(10));
+            } while (!stop);
+            Assert.True(data == content, "No Data received via Socket");
 
             protocol1.Stop();
             protocol.Stop();
