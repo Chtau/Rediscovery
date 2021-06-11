38e344bb5d1114187267f88d7d4586500714c718
Christoph Taucher <taucher.ch@gmail.com>
2021-06-11 10:55:05 +0200

    add unit test for send with proxy

---
+++ Rediscovery.Communication.Protocol.Test/SendReceiveWithProxy.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Rediscovery.Communication.Protocol.Test
+{
+    public class SendReceiveWithProxy
+    {
+        [Fact]
+        public void SendReceiveSimpleText()
+        {
+            string content = "Test";
+            IRediscoveryProtocol protocolProxy = Shared.TestDevice(new Models.ConnectionConfiguration(16576, 16577, 1024));
+            IRediscoveryProtocol protocol2 = new RediscoveryProtocol();
+            protocol2.Start(new Models.Configuration
+            {
+                Data = new Models.DataConfiguration
+                {
+                    Connection = new Models.ConnectionConfiguration(16586, 16587, 1024),
+                    ConnectionLargeData = new Models.ConnectionConfiguration(16488, 16489, 1024 * 60)
+                }
+            });
+
+            IRediscoveryProtocol protocol = new RediscoveryProtocol();
+            protocol.Start(null);
+            Task.Delay(TimeSpan.FromSeconds(1)).GetAwaiter().GetResult();
+
+            bool stop = false;
+            string data = null;
+            Task.Run(async () =>
+            {
+                protocol2.Listen<string>((transfer) =>
+                {
+                    data = transfer.Content;
+                    stop = true;
+                });
+                await Task.Delay(TimeSpan.FromMinutes(1));
+                stop = true;
+            });
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+            Task.Run(() =>
+            {
+                protocol.Send(new Transfer<string>(protocol2.Identifer, content));
+            });
+
+            do
+            {
+                System.Threading.Thread.Sleep(TimeSpan.FromMilliseconds(10));
+            } while (!stop);
+            Assert.True(data == content, "No Data received via Socket");
+
+            protocol2.Stop();
+            protocolProxy.Stop();
+            protocol.Stop();
+        }
+    }
+}
